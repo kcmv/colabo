f41705feea6d0c95d99ed2ea64f8e86b80b913fd
Sinisha Rudan <sinisa.rudan@gmail.com>
2015-03-29 22:48:13 +0200

    DB addition

---
+++ src/backend/KNode.js
@@ -0,0 +1,77 @@
+/**
+ * New node file
+ */
+var mongoose = require('mongoose');
+
+/* SCHEMA */
+var KNodeSchema = mongoose.Schema({
+	name: String,
+	iAmId: Number,
+	activeVersion: { type: Number, default: 1 },
+	ideaId: Number,
+	version: { type: Number, default: 1 }, //{type: DataTypes.INTEGER, allowNull: false, primaryKey: true},
+	isPublic: { type: Boolean, default: true },
+	createdAt: { type: Date, default: Date.now }, //CHECK AUTOMATIC OPTIONS
+	updatedAt: { type: Date, default: Date.now }
+	dataContentSerialized: String,
+	visual: {
+		isOpen: { type: Boolean, default: false },
+		manualX: Number,
+		manualY: Number
+	}
+});
+
+var KNodeModel = mongoose.model('KNode', KNodeSchema);
+
+module.exports = KNodeModel; //then we can use it by: var User = require('./app/models/KNodeModel');
+
+/* connecting */
+mongoose.connect('mongodb://localhost/KnAllEdge');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+
+/* CRUD */
+function create(knodeJSON){ //!!! but do we get object of type KNodeModel
+	var knode = new SLaWS(knodeJSON); //testirati
+	knode.save();
+	
+	knode.save(function(err) {
+		if (err) throw err;
+	});
+}
+
+function update(knode){
+	
+}
+
+userSchema.pre('save', function(next) {
+	  // get the current date
+	  var currentDate = new Date();
+	  
+	  // change the updated_at field to current date
+	  this.updated_at = currentDate;
+
+	  // if created_at doesn't exist, add to that field
+	  if (!this.created_at)
+	    this.created_at = currentDate;
+
+	  next();
+	});
+
+function read(id){
+	
+	//return KNodeModel.findById(id).exec().onResolve
+	
+	return KNodeModel.findById(id, function (err, knode) {
+		if (err) throw err;
+		else return knode;
+	});
+}
+
+exports.index = function(req, res){
+	
+}
+function delete(id){
+	
+}
---
+++ src/backend/KnAllEdgeBackend.js
@@ -0,0 +1,59 @@
+process.chdir(__dirname);
+
+var config = require('./config/global')
+  ,	async   = require('async')
+  , express = require('express')
+  , resource = require('express-resource')
+  , fs      = require('fs')
+  , http    = require('http')
+  , https   = require('https')
+  , flash 	 = require('connect-flash');
+ 
+console.log("[KnAllEdgeBackend.js:index] config.paths: %s", JSON.stringify(config.paths));
+console.log("[KnAllEdgeBackend.js:index] config.mockups: %s", JSON.stringify(config.mockups));
+console.log("[KnAllEdgeBackend.js:index] config.services: %s", JSON.stringify(config.services));
+
+// function supportCrossOriginScript(req, res, next) {
+// 	res.header("Access-Control-Allow-Headers", "Content-Type");
+// 	// res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// 	res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT, HEAD");
+// 	res.header("Allow", "POST, GET, OPTIONS, DELETE, PUT, HEAD");
+// 	// res.header("Access-Control-Max-Age","1728000");
+	
+// 	// res.header("Access-Control-Allow-Origin", "*");
+// 	// http://stackoverflow.com/questions/15026016/set-cookie-in-http-header-is-ignored-with-angularjs
+// 	var origin = req.headers.origin; // "api.transpoetika.org";
+// 	console.log("Access-Control-Allow-Origin: %s", origin);
+// 	//var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+// 	// console.log("Access-Control-Allow-Origin: %s", ip);
+// 	console.log("Access-Control-Allow-Origin: %s", origin);
+// 	res.header('Access-Control-Allow-Origin', origin);
+// 	res.header('Access-Control-Allow-Credentials', true);
+	
+// 	console.log("[supportCrossOriginScript] setting up headers");
+
+//     res.status(200);
+//     next();
+// }
+
+var app = express();
+
+app.configure(function(){
+    app.use(express.bodyParser());
+    app.use(express.logger());
+	app.use(express.cookieParser()); // cookie parser is used before the session
+	app.set('port', process.env.PORT || 8080);
+
+	// this is enough
+	//app.use(supportCrossOriginScript);
+	// so no need for this
+	// app.options('/iam/users', supportCrossOriginScript);
+
+    app.use(app.router);
+});
+
+var forms = app.resource('forms', require('./modules/form'), {id: 'type?/:searchParam?'});
+
+http.createServer(app).listen(app.get('port'), function() {
+	console.log("Listening on " + app.get('port'));
+});
