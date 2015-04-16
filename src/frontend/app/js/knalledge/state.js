252a76df70d749c1a3ccda42adabee318009c3ff
Sasha Mile Rudan <mprinc@gmail.com>
2015-04-16 22:57:24 +0200

    Added knalledge.State class

---
+++ src/frontend/app/js/knalledge/state.js
@@ -0,0 +1,10 @@
+(function () { // This prevents problems when concatenating scripts that aren't strict.
+'use strict';
+
+var State =  knalledge.State = function(storageApi){
+};
+
+State.prototype.init = function(){
+};
+
+}()); // end of 'use strict';
