b68eb5d9c7027b24d04dcb2a4919bed034a7cee3
Sasha Mile Rudan <mprinc@gmail.com>
2015-04-17 02:04:05 +0200

    Fixed KNode

--- src/frontend/app/js/knalledge/kNode.js
+++ src/frontend/app/js/knalledge/kNode.js
@@ -5,21 +5,21 @@ var KNode =  knalledge.KNode = function(){
 	this._id = 0; //maxId logic should be migrated here
 	this.name = "name...";
 	this.mapId = KNode.DEFAULT_MAP_ID;	
-	this.iAmId = 0,
-	this.activeVersion = 1,
-	this.ideaId = 0,
-	this.version = 1,
-	this.isPublic = true,
-	this.createdAt = null,
-	this.updatedAt = null,
-	this.dataContent = "",
+	this.iAmId = 0;
+	this.activeVersion = 1;
+	this.ideaId = 0;
+	this.version = 1;
+	this.isPublic = true;
+	this.createdAt = null;
+	this.updatedAt = null;
+	this.dataContent = "";
 	this.visual = {
-			isOpen = false,
-			x = 0,
-			y = 0,
-			width = 0,
-			height = 0
-	}
+			isOpen: false,
+			x: 0,
+			y: 0,
+			width: 0,
+			height: 0
+	};
 	
 	/* local-to-frontend */
 	this.state = KNode.STATE_LOCAL;
