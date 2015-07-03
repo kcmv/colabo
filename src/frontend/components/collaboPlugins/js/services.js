c29c07952abe5d87c943f13c68cacd70e64efaf6
Sasha Mile Rudan <mprinc@gmail.com>
2015-07-03 21:07:34 +0200

    Plugins fixes

--- src/frontend/components/collaboPlugins/js/services.js
+++ src/frontend/components/collaboPlugins/js/services.js
@@ -19,7 +19,7 @@ collaboPluginsServices.provider('CollaboPluginsService', function CollaboPlugins
 	var _componentsByPlugins = {};
 	var _references = {};
 	var _referencesByPlugins = {};
-	
+
 	this.setActive = function(isActive){
 		_isActive = isActive;
 	};
@@ -42,10 +42,51 @@ collaboPluginsServices.provider('CollaboPluginsService', function CollaboPlugins
 			}
 			var referenceByPlugins = _referencesByPlugins[referenceName];
 			referenceByPlugins.push(pluginOptions);
+			// distribute reference immediatelly if reference provider has already provide it
+			_distributeReferences(referenceName);
 		}
 	};
 
+	var _distributeReferences = function(referenceName){
+		var items = _references[referenceName];
+		if(!items) return;
+		var referenceByPlugins = _referencesByPlugins[referenceName];
+		if (!referenceByPlugins) return;
+		for(var i=0; i<referenceByPlugins.length; i++){
+			var pluginOptions = referenceByPlugins[i];
+			var pluginReferences = pluginOptions.references[referenceName];
+			console.log("[CollaboPluginsService::_distributeReferences] providing references to the plugin '%s'", pluginOptions.name);
+			if(!pluginReferences){
+				console.error("[CollaboPluginsService::_distributeReferences] missing reference '%s' for the plugin '%s' that registered for that reference?!", referenceName, pluginOptions.name);
+				continue;
+			}
+			for(var itemName in pluginReferences.items){
+				if(!(itemName in items)){
+					console.error("[CollaboPluginsService::_distributeReferences] plugin '%s' asked for the item '%s' that references provider '%s' doesn't provide?!", pluginOptions.name, itemName, referenceName);
+					continue;
+				}
+				pluginReferences.items[itemName] = items[itemName];
+			}
+			pluginReferences.resolved = true;
+			if(pluginReferences.callback) pluginReferences.callback();
+		}
+	};
+
+	var _provideReferences = function(referenceName, items){
+		console.log("[CollaboPluginsService::provideReferences] providing references from the reference provided '%s'", referenceName);
+		_references[referenceName] = items;
+
+		_distributeReferences(referenceName);
+	};
+
+	var _provideApi = function(apiName, api){
+		console.log("[CollaboPluginsService::provideReferences] providing api from the api provider: '%s'", apiName);
+		_apis[apiName] = api;
+	};
+
+	this.provideReferences = _provideReferences;
 	this.registerPlugin = _registerPlugin;
+	this.provideApi = _provideApi;
 
 	this.$get = ['$rootScope', 'ENV', /*'$q', */
 	function($rootScope, ENV /*$q */) {
@@ -63,35 +104,10 @@ collaboPluginsServices.provider('CollaboPluginsService', function CollaboPlugins
 			registerPlugin: _registerPlugin,
 
 			// reference provider provides references (with referenceName) for others to retrieve them
-			provideReferences: function(referenceName, items){
-				console.log("[CollaboPluginsService::provideReferences] providing references from the reference provided '%s'", referenceName);
-				var referenceByPlugins = _referencesByPlugins[referenceName];
-				if (referenceByPlugins == null) return;
-				for(var i=0; i<referenceByPlugins.length; i++){
-					var pluginOptions = referenceByPlugins[i];
-					var pluginReferences = pluginOptions.references[referenceName];
-					console.log("[CollaboPluginsService::provideReferences] providing references to the plugin '%s'", pluginOptions.name);
-					if(!pluginReferences){
-						console.error("[CollaboPluginsService::provideReferences] missing reference '%s' for the plugin '%s' that registered for that reference?!", referenceName, pluginOptions.name);
-						continue;
-					}
-					for(var itemName in pluginReferences.items){
-						if(!(itemName in items)){
-							console.error("[CollaboPluginsService::provideReferences] plugin '%s' asked for the item '%s' that references provider '%s' doesn't provide?!", pluginOptions.name, itemName, referenceName);
-							continue;
-						}
-						pluginReferences.items[itemName] = items[itemName];
-					}
-					pluginReferences.resolved = true;
-					if(pluginReferences.callback) pluginReferences.callback();
-				}
-			},
+			provideReferences: _provideReferences,
 
 			// api provider provides api (with apiName name) to be registered with the collaboplugins service and available for others to use it
-			provideApi: function(apiName, api){
-				console.log("[CollaboPluginsService::provideReferences] providing api from the api provider: '%s'", apiName);
-				_apis[apiName] = api;
-			},
+			provideApi: _provideApi,
 
 			// api consumers can ask for particular api
 			getApi: function(apiName){
