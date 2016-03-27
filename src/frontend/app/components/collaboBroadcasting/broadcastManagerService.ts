13d70e2ac9d5fb81672032a85967911900e08252
Sinisha Rudan <sinisa.rudan@gmail.com>
2016-03-27 22:08:44 +0200

    Broadcasting linking

--- src/frontend/app/components/collaboBroadcasting/broadcastManagerService.ts
+++ src/frontend/app/components/collaboBroadcasting/broadcastManagerService.ts
@@ -3,9 +3,23 @@ import {KnalledgeMapPolicyService} from  '../knalledgeMap/knalledgeMapPolicyServ
 export class BroadcastManagerService {
     private knalledgeMapPolicyService:KnalledgeMapPolicyService;
     private knAllEdgeRealTimeService;
+    public KnRealTimeNodeSelectedEventName:String = "node-selected";
 
     constructor(_KnalledgeMapPolicyService_, _KnAllEdgeRealTimeService_) {
         this.knalledgeMapPolicyService = _KnalledgeMapPolicyService_;
         this.knAllEdgeRealTimeService = _KnAllEdgeRealTimeService_;
+
+        var BroadcastManagerServicePluginOptions:Object = {
+    			name: "BroadcastManagerService",
+    			events: {
+    			}
+    		};
+    		BroadcastManagerServicePluginOptions.events[KnRealTimeNodeSelectedEventName] = this.receiveBroadcast.bind(this);
+    		this.knAllEdgeRealTimeService.registerPlugin(BroadcastManagerServicePluginOptions);
     }
+
+    receiveBroadcast:Function = function(user:Object){
+        console.log("[receiveBroadcast] result: ", result);
+        return;
+    };
 }
