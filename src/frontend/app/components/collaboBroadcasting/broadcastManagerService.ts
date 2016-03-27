97eb069cd656240fdf37d4b827296b37d45f9e0e
Sasha Mile Rudan <mprinc@gmail.com>
2016-03-27 21:31:56 +0200

    Added listener to kn:realtime for node-selected in 'components/collaboBroadcasting/broadcastManagerService.ts'

--- src/frontend/app/components/collaboBroadcasting/broadcastManagerService.ts
+++ src/frontend/app/components/collaboBroadcasting/broadcastManagerService.ts
@@ -3,9 +3,23 @@ import {KnalledgeMapPolicyService} from  '../knalledgeMap/knalledgeMapPolicyServ
 export class BroadcastManagerService {
     private knalledgeMapPolicyService:KnalledgeMapPolicyService;
     private knAllEdgeRealTimeService;
+    private knRealTimeNodeSelectedEventName:string = 'node-selected';
 
     constructor(_KnalledgeMapPolicyService_, _KnAllEdgeRealTimeService_) {
         this.knalledgeMapPolicyService = _KnalledgeMapPolicyService_;
         this.knAllEdgeRealTimeService = _KnAllEdgeRealTimeService_;
+
+        var broadcastManagerServicePluginOptions = {
+			name: "BroadcastManagerService",
+			events: {
+			}
+		};
+		broadcastManagerServicePluginOptions.events[this.knRealTimeNodeSelectedEventName] = this.receiveBroadcast.bind(this);
+		this.knAllEdgeRealTimeService.registerPlugin(broadcastManagerServicePluginOptions);
+    }
+
+    receiveBroadcast(eventName:string, msg:any) {
+    	console.log("[BroadcastManagerService:receiveBroadcast] (clientId:%s) eventName: %s, msg: %s",
+    	this.knAllEdgeRealTimeService.getClientInfo().clientId, eventName, JSON.stringify(msg));
     }
 }
