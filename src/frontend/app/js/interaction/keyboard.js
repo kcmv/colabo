608c373dd943e4cf6c39cc248fdfe346353cfcd0
Sasha Mile Rudan <mprinc@gmail.com>
2016-05-02 22:20:14 +0200

    Keyboard interface is replaced with new more library-generic implementation KeyboardJS library is replaced with Kibo.js library Problems with re-triggering is resolved TODO: test all key combinations if they are still working
    Closes #10

--- src/frontend/app/js/interaction/keyboard.js
+++ src/frontend/app/js/interaction/keyboard.js
@@ -1,8 +1,8 @@
 (function () { // This prevents problems when concatenating scripts that aren't strict.
 'use strict';
 
-//http://robertwhurst.github.io/KeyboardJS/
-//	https://github.com/RobertWHurst/KeyboardJS
+//http://robertwhurst.github.io/keyboardJS/
+//	https://github.com/RobertWHurst/keyboardJS
 
 /**
 @classdesc Provides keyboard interaction for the KnAllEdge system
@@ -23,7 +23,7 @@ var Keyboard =  interaction.Keyboard = function(mapInteraction){
  * listening for commands while we are typing a regular text inside the node name or content
  * @type {String}
  */
-Keyboard.KEY_PREFIX = "ctrl + ";
+Keyboard.KEY_PREFIX;
 
 /**
 * @var {debugPP} debug - namespaced debug for the class
@@ -35,211 +35,244 @@ Keyboard.prototype.init = function(){
 	this.initializeKeyboard();
 };
 
-// http://robertwhurst.github.io/KeyboardJS/
+// http://robertwhurst.github.io/keyboardJS/
 Keyboard.prototype.initializeKeyboard = function() {
 	//var that = this;
 
-	KeyboardJS.on("right", function(){
+	// var keyboardLibType = 'KeyboardJS';
+	var keyboardLibType = 'Kibo';
+
+	var keyboardLib;
+	var keyboardCommand;
+	var keyboardCommandUp;
+	var keyboardCommandDown;
+	var keyboardSequenceKey;
+
+	switch(keyboardLibType){
+		case 'KeyboardJS':
+			keyboardLib = keyboardJS;
+			keyboardCommand = 'bind';
+			keyboardSequenceKey = " + ";
+		break;
+		case 'Kibo':
+			keyboardLib = new Kibo();
+			keyboardCommand = 'down';
+			keyboardCommandUp = 'up';
+			keyboardCommandDown = 'down';
+			keyboardSequenceKey = " ";
+		break;
+	}
+
+	Keyboard.KEY_PREFIX = "ctrl" + keyboardSequenceKey;
+
+	keyboardLib[keyboardCommand]("right", function(){
 		this.mapInteraction.navigateRight();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on("left", function(){
+	keyboardLib[keyboardCommand]("left", function(){
 		this.mapInteraction.navigateLeft();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on("down", function(){
+	keyboardLib[keyboardCommand]("down", function(){
 		this.mapInteraction.navigateDown();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on("up", function(){
+	keyboardLib[keyboardCommand]("up", function(){
 		this.mapInteraction.navigateUp();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	/**
 	 * opening node
 	 */
 
-	KeyboardJS.on("alt + 1", function(){
-		console.log("alt + 1");
+	keyboardLib[keyboardCommand]("alt" + keyboardSequenceKey + "1", function(){
+		console.log("alt" + keyboardSequenceKey + "1");
 		this.mapInteraction.switchToMap();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on("alt + 2", function(){
-		console.log("alt + 2");
+	keyboardLib[keyboardCommand]("alt" + keyboardSequenceKey + "2", function(){
+		console.log("alt" + keyboardSequenceKey + "2");
 		this.mapInteraction.switchToProperties();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "enter", function(){
-		console.log("ctrl + enter");
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"enter", function(){
+		console.log("ctrl" + keyboardSequenceKey + "enter");
 		this.mapInteraction.toggleNode();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	/**
 	 * starting node editing
 	 */
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "space",
-		function(){
-			if(this.mapInteraction.isEditingNode()){
-				return;
-			}
-			if(!this.mapInteraction.isStatusMap()) return;
-			return false;
-		}.bind(this),
-		function(){
-			this.mapInteraction.setEditing();
-		}.bind(this),
-		function(){
+	var keyboardSpaceDown = function(){
+		if(this.mapInteraction.isEditingNode()){
+			return;
+		}
+		if(!this.mapInteraction.isStatusMap()) return;
+		return false;
+	}.bind(this);
 
-		}.bind(this)
-	);
+	var keyboardSpaceUp = function(){
+		this.mapInteraction.setEditing();
+	}.bind(this);
+
+	if(keyboardCommandUp && keyboardCommandDown){
+		keyboardLib[keyboardCommandDown](
+			Keyboard.KEY_PREFIX+"space", keyboardSpaceDown);
+		keyboardLib[keyboardCommandUp](
+			Keyboard.KEY_PREFIX+"space", keyboardSpaceUp);
+	}else{
+		keyboardLib[keyboardCommand](
+			Keyboard.KEY_PREFIX+"space", keyboardSpaceDown,
+			Keyboard.KEY_PREFIX+"space", keyboardSpaceUp);
+	}
 
 	/**
 	 * search for the node name
 	 */
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "f", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"f", function(){
 		if(this.mapInteraction.isEditingNode()) return;
 		this.mapInteraction.searchNodeByName();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	/**
 	 * toggling moderator
 	 */
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "alt + m", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"alt" + keyboardSequenceKey + "m", function(){
 		if(this.mapInteraction.isEditingNode()) return;
 
 		this.mapInteraction.toggleModerator();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	/**
 	 * toggling presenter
 	 */
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "alt + p", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"alt" + keyboardSequenceKey + "p", function(){
 		if(this.mapInteraction.isEditingNode()) return;
 
 		this.mapInteraction.togglePresenter();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	/**
 	 * finishing node editing
 	 */
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "escape", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"escape", function(){
 		console.log("editing escaping");
 		this.mapInteraction.exitEditingNode();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	// IBIS
 	// Vote up
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "command + up", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"command" + keyboardSequenceKey + "up", function(){
 		if(this.mapInteraction.isEditingNode()) return;
 
 		this.mapInteraction.nodeVoteUp();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	// Vote up
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "command + down", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"command" + keyboardSequenceKey + "down", function(){
 		if(this.mapInteraction.isEditingNode()) return;
 
 		this.mapInteraction.nodeVoteDown();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	// Add Image
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "i", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"i", function(){
 		// window.prompt("Kmek");
 		if(this.mapInteraction.isEditingNode()) return;
 
 		this.mapInteraction.addImage();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	// Remove Image
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "shift + i", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"shift" + keyboardSequenceKey + "i", function(){
 		if(this.mapInteraction.isEditingNode()) return;
 
 		this.mapInteraction.removeImage();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	// Add Link
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "l", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"l", function(){
 		if(this.mapInteraction.isEditingNode()) return;
 
 		this.mapInteraction.addLink();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	// Add new child node
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "n", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"n", function(){
 		if(!this.mapInteraction.isStatusMap()) return;
 		this.mapInteraction.addChildNode();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "alt + 1", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"alt" + keyboardSequenceKey + "1", function(){
 		if(!this.mapInteraction.isStatusMap()) return;
 		this.mapInteraction.addChildNode(knalledge.KNode.TYPE_IBIS_QUESTION, knalledge.KEdge.TYPE_IBIS_QUESTION);
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "alt + 2", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"alt" + keyboardSequenceKey + "2", function(){
 		if(!this.mapInteraction.isStatusMap()) return;
 		this.mapInteraction.addChildNode(knalledge.KNode.TYPE_IBIS_IDEA, knalledge.KEdge.TYPE_IBIS_IDEA);
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "alt + 3", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"alt" + keyboardSequenceKey + "3", function(){
 		if(!this.mapInteraction.isStatusMap()) return;
 		this.mapInteraction.addChildNode(knalledge.KNode.TYPE_IBIS_ARGUMENT, knalledge.KEdge.TYPE_IBIS_ARGUMENT);
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "alt + 4", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"alt" + keyboardSequenceKey + "4", function(){
 		if(!this.mapInteraction.isStatusMap()) return;
 		this.mapInteraction.addChildNode(knalledge.KNode.TYPE_IBIS_COMMENT, knalledge.KEdge.TYPE_IBIS_COMMENT);
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	// Add new sibling node
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "shift + n", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"shift" + keyboardSequenceKey + "n", function(){
 		if(!this.mapInteraction.isStatusMap()) return;
 		this.mapInteraction.addSiblingNode();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "shift + alt + 1", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"shift" + keyboardSequenceKey + "alt" + keyboardSequenceKey + "1", function(){
 		if(!this.mapInteraction.isStatusMap()) return;
 		this.mapInteraction.addSiblingNode(knalledge.KNode.TYPE_IBIS_QUESTION, knalledge.KEdge.TYPE_IBIS_QUESTION);
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "shift + alt + 2", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"shift" + keyboardSequenceKey + "alt" + keyboardSequenceKey + "2", function(){
 		if(!this.mapInteraction.isStatusMap()) return;
 		this.mapInteraction.addSiblingNode(knalledge.KNode.TYPE_IBIS_IDEA, knalledge.KEdge.TYPE_IBIS_IDEA);
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "shift + alt + 3", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"shift" + keyboardSequenceKey + "alt" + keyboardSequenceKey + "3", function(){
 		if(!this.mapInteraction.isStatusMap()) return;
 		this.mapInteraction.addSiblingNode(knalledge.KNode.TYPE_IBIS_ARGUMENT, knalledge.KEdge.TYPE_IBIS_ARGUMENT);
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "shift + alt + 4", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"shift" + keyboardSequenceKey + "alt" + keyboardSequenceKey + "4", function(){
 		if(!this.mapInteraction.isStatusMap()) return;
 		this.mapInteraction.addSiblingNode(knalledge.KNode.TYPE_IBIS_COMMENT, knalledge.KEdge.TYPE_IBIS_COMMENT);
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	// Relinks the node:
 	// PreAction: select the node to relink
 	// PostAction: select new parent node
 	//TODO: this UI will not work when we will have more parents of node!
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "k", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"k", function(){
 		this.mapInteraction.relinkNode();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	/**
 	 *  Activates Request console for participant:
 	* PreAction: ...
 	* PostAction: ...
 	*/
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "r", function(){
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"r", function(){
 		this.mapInteraction.participantRequest();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	// Delete node:
-	KeyboardJS.on(Keyboard.KEY_PREFIX + "delete", function(){
-		console.log("ctrl + delete");
+	keyboardLib[keyboardCommand](Keyboard.KEY_PREFIX+"delete", function(){
+		console.log("ctrl" + keyboardSequenceKey + "delete");
 		if(this.mapInteraction.isEditingNode()) return; // in typing mode
 
 		this.mapInteraction.deleteNode();
-	}.bind(this), function(){}.bind(this));
+	}.bind(this));
 
 	//TODO: Delete edge
 };
