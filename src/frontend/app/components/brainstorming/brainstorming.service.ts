8aa0318d9169353d48bb69d066de7346b32153e5
Sasha Mile Rudan <mprinc@gmail.com>
2016-07-19 00:27:10 -0600

    integrated support for accessing plugins in brainstorming with example of isSelectedNodeIbisQuestion()

--- src/frontend/app/components/brainstorming/brainstorming.service.ts
+++ src/frontend/app/components/brainstorming/brainstorming.service.ts
@@ -7,30 +7,29 @@ import {Change, ChangeType, Domain, Event} from '../change/change';
 @Injectable()
 export class BrainstormingService {
     brainstorming: Brainstorming = new Brainstorming();
-    private brainstormingPluginInfo:any;
-    private collaboPluginsService:any=null;
+    private brainstormingPluginInfo: any;
     private knAllEdgeRealTimeService: any;
-  /**
-   * Service constructor
-   * @constructor
-   */
-  constructor(
-      @Inject('$injector') private $injector,
-      //  @Inject('RimaService') private rimaService,
-      // @Inject('KnalledgeMapVOsService') private knalledgeMapVOsService,
-      @Inject('GlobalEmitterServicesArray') private globalEmitterServicesArray: GlobalEmitterServicesArray//,
-      // , @Inject('CollaboPluginsService') private collaboPluginsService: CollaboPluginsService
-
-      ) {
+    /**
+     * Service constructor
+     * @constructor
+     */
+    constructor(
+        @Inject('$injector') private $injector,
+        //  @Inject('RimaService') private rimaService,
+        // @Inject('KnalledgeMapVOsService') private knalledgeMapVOsService,
+        @Inject('GlobalEmitterServicesArray') private globalEmitterServicesArray: GlobalEmitterServicesArray//,
+        , @Inject('CollaboPluginsService') private collaboPluginsService
+        ) {
+        let that = this;
         this.knAllEdgeRealTimeService = this.$injector.get('KnAllEdgeRealTimeService');
         let requestPluginOptions: any = {
             name: "RequestService",
             events: {
             }
         };
-        if(this.knAllEdgeRealTimeService){
-          requestPluginOptions.events[Event.BRAINSTORMING_CHANGED] = this.receivedBrainstormingChange.bind(this);
-          this.knAllEdgeRealTimeService.registerPlugin(requestPluginOptions);
+        if (this.knAllEdgeRealTimeService) {
+            requestPluginOptions.events[Event.BRAINSTORMING_CHANGED] = this.receivedBrainstormingChange.bind(this);
+            this.knAllEdgeRealTimeService.registerPlugin(requestPluginOptions);
         }
 
         //this.collaboPluginsService = this.$injector.get('CollaboPluginsService');
@@ -74,61 +73,70 @@ export class BrainstormingService {
         };
 
         // this.brainstormingPluginInfo.references.map.$promise = $q(function(resolve, reject) { /*jshint unused:false*/
-        //     this.brainstormingPluginInfo.references.map.callback = function() {
-        //         this.brainstormingPluginInfo.references.map.$resolved = true;
-        //         resolve(this.brainstormingPluginInfo.references.map);
-        //         // reject('not allowed');
-        //     };
+        this.brainstormingPluginInfo.references.map.callback = function() {
+            that.brainstormingPluginInfo.references.map.$resolved = true;
+            // resolve(that.brainstormingPluginInfo.references.map);
+            // reject('not allowed');
+        };
         // });
         //
         // this.brainstormingPluginInfo.apis.map.$promise = $q(function(resolve, reject) { /*jshint unused:false*/
-        //     this.brainstormingPluginInfo.apis.map.callback = function() {
-        //         this.brainstormingPluginInfo.apis.map.$resolved = true;
-        //         resolve(this.brainstormingPluginInfo.apis.map);
-        //         // reject('not allowed');
-        //     };
+        this.brainstormingPluginInfo.apis.map.callback = function() {
+            that.brainstormingPluginInfo.apis.map.$resolved = true;
+            // resolve(that.brainstormingPluginInfo.apis.map);
+            // reject('not allowed');
+        };
         // });
         //
-        // this.collaboPluginsService.registerPlugin(this.brainstormingPluginInfo);
-  }
 
-  sendBrainstorming(callback: Function) {
+        this.collaboPluginsService.registerPlugin(this.brainstormingPluginInfo);
+    }
 
-      // this.brainstorming.mapId = this.knalledgeMapVOsService.getMapId();
-      // this.brainstorming.who = this.rimaService.getWhoAmI()._id;
-      console.log(this.brainstorming);
+    isSelectedNodeIbisQuestion(): boolean {
+        if (!this.brainstormingPluginInfo.references.map.$resolved) return false;
 
-      if (this.knAllEdgeRealTimeService) {
-        let change = new Change();
-        change.value = this.brainstorming;
-        change.reference = this.brainstorming.question;
-        change.type = ChangeType.BEHAVIORAL;
-        change.domain = Domain.GLOBAL;
-        change.event = Event.BRAINSTORMING_CHANGED;
-        this.knAllEdgeRealTimeService.emit(change.event, change);
-        callback(true);
-      } else {
-          callback(false, 'SERVICE_UNAVAILABLE');
-      }
-  }
+        var vkNode = this.brainstormingPluginInfo.references.map.items.mapStructure.getSelectedNode();
+        if(!vkNode) return false;
+        return (vkNode.kNode.type === 'type_ibis_question');
+    }
 
-  private receivedBrainstormingChange(event: string, change:Change){
-    let receivedBrainstorming:Brainstorming = Brainstorming.factory(change.value);
-    console.warn("[receivedBrainstormingChange]receivedBrainstorming: ",receivedBrainstorming);
+    sendBrainstorming(callback: Function) {
 
-    // how it was earlier in knalledgeMap/directives.js (TO REMOVE FROM THERE AND FROM OTHER PLACES OLD LOGICS)
-    // var realTimeBehaviourChanged = function(eventName, msg){
-    //   console.log('realTimeBehaviourChanged:', eventName,'msg:', msg);
-    //
-    //   switch(msg.path){
-    //     case 'policyConfig.behaviour.brainstorming':
-    //       KnalledgeMapPolicyService.provider.config.behaviour.brainstorming = msg.value;
-    //       break;
-    //   }
-    //   updateState(msg.value);
-    //   $scope.knalledgeMap.update();
-    // }
-  }
+        // this.brainstorming.mapId = this.knalledgeMapVOsService.getMapId();
+        // this.brainstorming.who = this.rimaService.getWhoAmI()._id;
+        console.log(this.brainstorming);
+
+        if (this.knAllEdgeRealTimeService) {
+            let change = new Change();
+            change.value = this.brainstorming;
+            change.reference = this.brainstorming.question;
+            change.type = ChangeType.BEHAVIORAL;
+            change.domain = Domain.GLOBAL;
+            change.event = Event.BRAINSTORMING_CHANGED;
+            this.knAllEdgeRealTimeService.emit(change.event, change);
+            callback(true);
+        } else {
+            callback(false, 'SERVICE_UNAVAILABLE');
+        }
+    }
+
+    private receivedBrainstormingChange(event: string, change: Change) {
+        let receivedBrainstorming: Brainstorming = Brainstorming.factory(change.value);
+        console.warn("[receivedBrainstormingChange]receivedBrainstorming: ", receivedBrainstorming);
+
+        // how it was earlier in knalledgeMap/directives.js (TO REMOVE FROM THERE AND FROM OTHER PLACES OLD LOGICS)
+        // var realTimeBehaviourChanged = function(eventName, msg){
+        //   console.log('realTimeBehaviourChanged:', eventName,'msg:', msg);
+        //
+        //   switch(msg.path){
+        //     case 'policyConfig.behaviour.brainstorming':
+        //       KnalledgeMapPolicyService.provider.config.behaviour.brainstorming = msg.value;
+        //       break;
+        //   }
+        //   updateState(msg.value);
+        //   $scope.knalledgeMap.update();
+        // }
+    }
 
 
 };
