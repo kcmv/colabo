b84ce2c1b0fc168be4ac47606fc4070645012771
Sasha Mile Rudan <mprinc@gmail.com>
2016-07-20 19:48:41 -0600

    Rewritten backend and frontend deployment scripts

--- src/frontend/scripts/_build-frontend.sh
+++ src/frontend/scripts/_build-frontend.sh
@@ -1,8 +1,8 @@
 #! /bin/bash -u
 #
-echo "[build] building started."
-echo "[build] Zip name: $zipname"
-echo "[build] Building project ..."
+echo "[build:$deploy_folder_sub] building started."
+echo "[build:$deploy_folder_sub] Zip name: $zipname"
+echo "[build:$deploy_folder_sub] Building project ..."
 npm run build.prod
 
 # Copying missing fonts
@@ -10,9 +10,9 @@ cp ./node_modules/ng2-material/font/MaterialIcons-Regular* dist/prod/css/
 mkdir -p dist/prod/fonts/
 cp -r ./app/fonts/font-awesome dist/prod/fonts/font-awesome/
 
-echo "[build] Zipping build into $zipname"
+echo "[build:$deploy_folder_sub] Zipping build into $zipname"
 zipcommand="zip -r -X $zipname dist/prod"
 # http://stackoverflow.com/questions/4668640/how-to-execute-command-stored-in-a-variable
 eval "$zipcommand"
-echo "[build] building finished. zipped into: $zipname."
+echo "[build:$deploy_folder_sub] building finished. zipped into: $zipname."
 say -v Whisper "project is built and zipped!"
