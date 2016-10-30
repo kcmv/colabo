96df87307d46cf9684f140a7f2c9022b9d967bc9
Sasha Mile Rudan <mprinc@gmail.com>
2016-10-30 17:49:57 +0100

    upgradeAdapter is migrated to use fake (empty) @NgModule

--- src/frontend/app/js/upgrade_adapter.ts
+++ src/frontend/app/js/upgrade_adapter.ts
@@ -4,55 +4,10 @@
 
 import {UpgradeAdapter} from '@angular/upgrade';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import {HttpModule} from '@angular/http';
-import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
-
-import {MaterialModule} from '@angular/material';
-// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {Ng2MaterialModule} from 'ng2-material';
-
-// providers
-import {ChangeService} from '../components/change/change.service';
-// upgradeAdapter.addProvider(ChangeService);
-import {CollaboGrammarService} from '../components/collaboPlugins/CollaboGrammarService';
-import {BrainstormingService} from '../components/brainstorming/brainstorming.service';
-import {SessionService} from '../components/session/session.service';
-
-var moduleProviders = [
-  // MATERIAL_PROVIDERS,
-  // DbAuditService,
-  // ChangeService
-  // provideRouter
-  // RequestService
-  // ROUTER_PROVIDERS
-  // BrainstormingService
-
-    // ChangeService,
-    // CollaboGrammarService,
-    // BrainstormingService,
-    // SessionService
-];
-
+// Create a fake @NgModule that can be later replaced with real @NgModule
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    // RouterModule.forRoot(DEMO_APP_ROUTES),
-    MaterialModule.forRoot(),
-    Ng2MaterialModule
-  ],
-  declarations: [
-    // DemoApp,
-  ],
-  providers: moduleProviders,
-  // entryComponents: [
-  //   // DemoApp,
-  // ],
 })
-export class AppModule {}
+export class AppModuleFake {}
 
-export const upgradeAdapter = new UpgradeAdapter(AppModule);
+export const upgradeAdapter = new UpgradeAdapter(AppModuleFake);
