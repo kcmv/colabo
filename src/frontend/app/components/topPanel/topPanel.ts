d54534422874131b7955b3d789f43bb5a13df2b1
Sasha Mile Rudan <mprinc@gmail.com>
2016-11-01 14:45:14 +0100

    Unlocked more of the code for the topPanel

--- src/frontend/app/components/topPanel/topPanel.ts
+++ src/frontend/app/components/topPanel/topPanel.ts
@@ -1,19 +1,19 @@
 import {Component, ViewEncapsulation, Inject, Input} from '@angular/core';
-// import { DatePipe } from "@angular/common";
-// import { OrderArrayPipe } from "../utils/orderArrayPipe";
-// import {RequestComponent} from '../request/request.component';
-// import {SuggestionComponent} from '../suggestion/suggestion.component';
-// import {ChangeComponent} from '../change/change.component';
-// import {ChangeService} from '../change/change.service';
-// import {SuggestionService} from '../suggestion/suggestion.service';
-// import {RequestService} from '../request/request.service';
+import { DatePipe } from "@angular/common";
+import { OrderArrayPipe } from "../utils/orderArrayPipe";
+import {RequestComponent} from '../request/request.component';
+import {SuggestionComponent} from '../suggestion/suggestion.component';
+import {ChangeComponent} from '../change/change.component';
+import {ChangeService} from '../change/change.service';
+import {SuggestionService} from '../suggestion/suggestion.service';
+import {RequestService} from '../request/request.service';
 import {upgradeAdapter} from '../../js/upgrade_adapter';
-// import {KnalledgeMapPolicyService} from '../knalledgeMap/knalledgeMapPolicyService';
+import {KnalledgeMapPolicyService} from '../knalledgeMap/knalledgeMapPolicyService';
 
 export interface ITabData {
   title: string;
   content: string;
-  service?: any;
+  service: any;
   newItems?: number;
   totalItems?: number;
   disabled?: boolean;
@@ -57,7 +57,7 @@ export class TopPanel {
       {
         title: 'suggestions',
         content: '<suggestion-component></suggestion-component>',
-        // service: this.suggestionService,
+        service: this.suggestionService,
         newItems: 0,
         totalItems: 0,
         opened: false
@@ -75,7 +75,7 @@ export class TopPanel {
         content: "<change-component (newChange)='updateChangesNo($event)' [initializeWithPreviousChanges]='true' [followChanges]='true'>"
         +
         "</change-component>",
-        // service: this.changeService,
+        service: this.changeService,
         newItems: 0,
         totalItems: 0,
         opened: false
@@ -83,7 +83,7 @@ export class TopPanel {
       {
         title: 'requests',
         content: '<request></request>',
-        // service: this.requestService,
+        service: this.requestService,
         newItems: 0,
         totalItems: 0,
         opened: false
@@ -93,28 +93,28 @@ export class TopPanel {
     //private _selectedIndex: number;
 
     constructor(
-        // @Inject('KnalledgeMapPolicyService') knalledgeMapPolicyService:KnalledgeMapPolicyService,
-        // private changeService: ChangeService,
-        // @Inject('SuggestionService') private suggestionService:SuggestionService,
-        // @Inject('RequestService') private requestService:RequestService
+        @Inject('KnalledgeMapPolicyService') knalledgeMapPolicyService:KnalledgeMapPolicyService,
+        private changeService: ChangeService,
+        @Inject('SuggestionService') private suggestionService:SuggestionService,
+        @Inject('RequestService') private requestService:RequestService
     ) {
-        // this.policyConfig = knalledgeMapPolicyService.get().config;
+        this.policyConfig = knalledgeMapPolicyService.get().config;
         // alert("this.policyConfig.moderating.enabled: "+this.policyConfig.moderating.enabled);
         // alert("policyConfig.broadcasting.enabled: "+this.policyConfig.broadcasting.enabled);
         //this.changeService.onChangeHandler =
-        // for(var i:number = 0; i< this.tabData.length; i++){
-        //   var tab = this.tabData[i];
-        //   if(tab.service &&
-        //   'onChangeHandler' in tab.service //typeof tab.service.onChangeHandler === ‘function’ cannot be used ...
-        //   //because `typeof tab.service.onChangeHandler` returns 'undefined'
-        //   ){
-        //     var that:TopPanel = this;
-        //     var tabIndex: number = i;
-        //     tab.service.onChangeHandler = function(no){
-        //       that.updateChangesNo(no,tabIndex);
-        //     };
-        //   }
-        // }
+        for(var i:number = 0; i< this.tabData.length; i++){
+          var tab = this.tabData[i];
+          if(tab.service &&
+          'onChangeHandler' in tab.service //typeof tab.service.onChangeHandler === ‘function’ cannot be used ...
+          //because `typeof tab.service.onChangeHandler` returns 'undefined'
+          ){
+            var that:TopPanel = this;
+            var tabIndex: number = i;
+            tab.service.onChangeHandler = function(no){
+              that.updateChangesNo(no,tabIndex);
+            };
+          }
+        }
     };
 
     // @Input()
@@ -129,57 +129,57 @@ export class TopPanel {
     //   return this._selectedIndex;
     // }
     //
-    // getTitle(tabIndex: number): string {
-    //   return this.tabData[tabIndex].title + (this.tabData[tabIndex].newItems > 0 ?
-    //     ' (' + this.tabData[tabIndex].newItems + '/' + this.tabData[tabIndex].totalItems + ')' : '');
+    getTitle(tabIndex: number): string {
+      return this.tabData[tabIndex].title + (this.tabData[tabIndex].newItems > 0 ?
+        ' (' + this.tabData[tabIndex].newItems + '/' + this.tabData[tabIndex].totalItems + ')' : '');
+    }
+
+    isVisible(tabIndex: number): boolean {
+      return this.tabData[tabIndex].opened;
+      // return this.tabData[tabIndex].title + (this.tabData[tabIndex].newItems > 0 ?
+      //   ' (' + this.tabData[tabIndex].newItems + '/' + this.tabData[tabIndex].totalItems + ')' : '');
+    }
+
+    focusChanged(tabIndex){
+      console.log("focusChanged", tabIndex);
+      //this._selectedIndex = tabIndex;
+      this.tabData[tabIndex].newItems = 0;
+      this.tabData[tabIndex].opened = !this.tabData[tabIndex].opened;
+      for(let i:number = 0; i < this.tabData.length; i++){
+        if(i !== tabIndex){
+          this.tabData[i].opened = false;
+        }
+      }
+    }
+
+    selectedChanged(event){
+      console.log("selectedChanged", event);
+      //this._selectedIndex = event;
+    }
+
+    // addTab(title, view) {
+    //   view = view || title + ' Content View';
+    //   this.tabs.push({title: title, content: view, disabled: false});
     // }
-    //
-    // isVisible(tabIndex: number): boolean {
-    //   return this.tabData[tabIndex].opened;
-    //   // return this.tabData[tabIndex].title + (this.tabData[tabIndex].newItems > 0 ?
-    //   //   ' (' + this.tabData[tabIndex].newItems + '/' + this.tabData[tabIndex].totalItems + ')' : '');
+
+    // removeTab(tab: ITabData) {
+    //   var index = this.tabs.indexOf(tab);
+    //   this.tabs.splice(index, 1);
+    //   this.selectedIndex = Math.min(index, this.tabs.length - 1);
     // }
-    //
-    // focusChanged(tabIndex){
-    //   console.log("focusChanged", tabIndex);
-    //   //this._selectedIndex = tabIndex;
-    //   this.tabData[tabIndex].newItems = 0;
-    //   this.tabData[tabIndex].opened = !this.tabData[tabIndex].opened;
-    //   for(let i:number = 0; i < this.tabData.length; i++){
-    //     if(i !== tabIndex){
-    //       this.tabData[i].opened = false;
-    //     }
-    //   }
-    // }
-    //
-    // selectedChanged(event){
-    //   console.log("selectedChanged", event);
-    //   //this._selectedIndex = event;
-    // }
-    //
-    // // addTab(title, view) {
-    // //   view = view || title + ' Content View';
-    // //   this.tabs.push({title: title, content: view, disabled: false});
-    // // }
-    //
-    // // removeTab(tab: ITabData) {
-    // //   var index = this.tabs.indexOf(tab);
-    // //   this.tabs.splice(index, 1);
-    // //   this.selectedIndex = Math.min(index, this.tabs.length - 1);
-    // // }
-    //
-    // opened() {
-    //   console.log("top panel is opened");
-    // }
-    //
-    // updateChangesNo(no: number, tabIndex:number){
-    //   console.log("[updateChangesNo] event:", no);
-    //   if(tabIndex < this.tabData.length){
-    //     this.tabData[tabIndex].totalItems +=no;
-    //     this.tabData[tabIndex].newItems +=no;
-    //   }
-    //   //this.changesTitle = (no > 0) ? (ChangeComponent.TITLE + " (" + no + ")") : ChangeComponent.TITLE;
-    // }
+
+    opened() {
+      console.log("top panel is opened");
+    }
+
+    updateChangesNo(no: number, tabIndex:number){
+      console.log("[updateChangesNo] event:", no);
+      if(tabIndex < this.tabData.length){
+        this.tabData[tabIndex].totalItems +=no;
+        this.tabData[tabIndex].newItems +=no;
+      }
+      //this.changesTitle = (no > 0) ? (ChangeComponent.TITLE + " (" + no + ")") : ChangeComponent.TITLE;
+    }
 }
 
 import {PluginsPreloader} from '../collaboPlugins/pluginsPreloader';
@@ -189,7 +189,6 @@ var componentDirectives = [
   // SuggestionComponent,
   // ChangeComponent,
   // upgradeAdapter.upgradeNg1Component('rimaRelevantList'),
-  // TopPanel,
 
   // pipes
   // DatePipe, OrderArrayPipe
