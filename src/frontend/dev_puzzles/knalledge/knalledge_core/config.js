9e9f517423f6221ffc7de9c92a6f5c37bfbf756b
Sasha Rudan <mprinc@gmail.com>
2017-11-29 18:18:08 +0300

    Migrated debugpp from bower into knalledge_core component.
    TODO: debugpp should be updated and migrated back to npm dependency

--- src/frontend/dev_puzzles/knalledge/knalledge_core/config.js
+++ src/frontend/dev_puzzles/knalledge/knalledge_core/config.js
@@ -21,6 +21,8 @@ var puzzles = {
         'code/knalledge/kNode.js', 'code/knalledge/kEdge.js', 'code/knalledge/kMap.js', 'code/knalledge/vkNode.js', 'code/knalledge/vkEdge.js',
         // TODO: Not sure where it fits (probable not here in the core) ...
         'code/knalledge/state.js',
+
+        'lib/debugpp/index.js'
       ],
       injectCss: ''
     }
