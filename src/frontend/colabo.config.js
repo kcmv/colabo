bba47703d103664a4594c255df3250ec629f5add
Sasha Rudan <mprinc@gmail.com>
2017-11-30 16:18:31 +0300

    Added dependency and offer puzzles in the

---
+++ src/frontend/colabo.config.js
@@ -0,0 +1,36 @@
+var puzzles = {
+  dependencies: {
+    "@colabo-puzzles/puzzles_core": {
+    },
+    "@colabo-knalledge/knalledge_core": {
+    },
+    "@colabo-knalledge/knalledge_store_core": {
+    },
+    "@colabo-knalledge/knalledge_view_enginee": {
+    },
+    "@colabo-knalledge/knalledge_view_interaction": {
+    }
+  },
+  offers: {
+    "@colabo-puzzles/puzzles_core": {
+      npm: "@colabo-puzzles/puzzles_core",
+      path: "dev_puzzles/puzzles/puzzles_core"
+    },
+    "@colabo-knalledge/knalledge_core": {
+      npm: "@colabo-knalledge/knalledge_core",
+      path: "dev_puzzles/knalledge/knalledge_core"
+    },
+    "@colabo-knalledge/knalledge_store_core": {
+      npm: "@colabo-knalledge/knalledge_store_core",
+      path: "dev_puzzles/knalledge/knalledge_store_core"
+    },
+    "@colabo-knalledge/knalledge_view_enginee": {
+      npm: "@colabo-knalledge/knalledge_view_enginee",
+      path: "dev_puzzles/knalledge/knalledge_view_enginee"
+    },
+    "@colabo-knalledge/knalledge_view_interaction": {
+      npm: "@colabo-knalledge/knalledge_view_interaction",
+      path: "dev_puzzles/knalledge/knalledge_view_interaction"
+    }
+  }
+}
