bc799976f3169aeedf89d49971b0f5b256d89c15
Sinisa Rudan <sinisa.rudan@gmail.com>
2018-04-07 17:20:39 +0200

    profilingState changing; receiving data; creating Node user

--- src/frontend/demos/rima/rima-colaboware/src/app/users-profiling/users-profiling.component.ts
+++ src/frontend/demos/rima/rima-colaboware/src/app/users-profiling/users-profiling.component.ts
@@ -1,6 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {CoLaboWareData} from './coLaboWareData';
 import {CoLaboWareType} from './coLaboWareData';
+
+import {MatRadioModule} from '@angular/material/radio';
+
+import {KNode} from '@colabo-knalledge/knalledge_core/code/knalledge/kNode';
+
+enum ProfilingStateType {
+  OFF = 'OFF',
+  USER_ID = 'USER_ID',
+  ATTRIBUTE = 'ATTRIBUTE'
+}
 
 @Component({
   selector: 'app-users-profiling',
@@ -8,6 +18,18 @@ import {CoLaboWareType} from './coLaboWareData';
   styleUrls: ['./users-profiling.component.css']
 })
 export class UsersProfilingComponent implements OnInit {
+
+  ProfilingStateTypeNames:String[] = [
+    'OFF',
+    'USER_ID',
+    'ATTRIBUTE'
+  ];
+
+  @Input() cw_data:string = '0009610521';
+
+  users:KNode[] = [];
+
+  profilingState: String = ProfilingStateType.OFF;
 
   constructor() { }
 
@@ -17,22 +39,55 @@ export class UsersProfilingComponent implements OnInit {
   profileNewUser():void
   {
     console.log('profileNewUser');
+    this.profilingState = ProfilingStateType.USER_ID;
+  }
+
+  setUserId(id:String):void
+  {
+    console.log('setUserId');
+    //TODO: +mprinc check if user exists
+    this.createUser(id);
+  }
+
+  createUser(id:String):void{
+    console.log('createUser');
+    let user:KNode = new KNode();
+    user.type = KNode.TYPE_USER;
+    this.users.push(user);
+    this.profilingState = ProfilingStateType.ATTRIBUTE;
+    //set RFID: = id;
+  }
+
+  setUserAttribute(attr:String):void{
+
   }
 
   inputUserProfile():void{
-
+    console.log('inputUserProfile');
   }
 
   sendDemoColabowareInput():void{
       let cwData = new CoLaboWareData();
       cwData.type = CoLaboWareType.RFID;
-      cwData.value = '0009610521';
+      cwData.value = this.cw_data;
       this.colabowareInput(cwData);
   }
 
   colabowareInput(cwData:CoLaboWareData){
     if(cwData.type === CoLaboWareType.RFID){
         console.log('cwData:'+cwData.value);
+        switch(this.profilingState){
+          case ProfilingStateType.USER_ID:
+            this.setUserId(cwData.value);
+            break;
+          case ProfilingStateType.ATTRIBUTE:
+            this.setUserAttribute(cwData.value);
+            break;
+
+          case ProfilingStateType.OFF:
+          default:
+            break;
+        }
     }
   }
 
