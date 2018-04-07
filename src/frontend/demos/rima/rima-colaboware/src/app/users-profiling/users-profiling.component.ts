630bfc312dbab9c75256d3eaeee316bea7db5356
Sasha Rudan <mprinc@gmail.com>
2018-04-07 12:56:08 -0400

    Added support for reading map, nodes and edges

--- src/frontend/demos/rima/rima-colaboware/src/app/users-profiling/users-profiling.component.ts
+++ src/frontend/demos/rima/rima-colaboware/src/app/users-profiling/users-profiling.component.ts
@@ -1,8 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+
+import {KMap} from '@colabo-knalledge/knalledge_core/code/knalledge/kMap';
+import {KEdge} from '@colabo-knalledge/knalledge_core/code/knalledge/kEdge';
+import {KNode} from '@colabo-knalledge/knalledge_core/code/knalledge/kNode';
+
+import {KnalledgeEdgeService} from '@colabo-knalledge/knalledge_store_core/knalledge-edge.service';
+import {KnalledgeNodeService} from '@colabo-knalledge/knalledge_store_core/knalledge-node.service';
+import {KnalledgeMapService} from '@colabo-knalledge/knalledge_store_core/knalledge-map.service';
+
 import { CoLaboWareType } from '@colabo-colaboware/colaboware_core/coLaboWareData';
 import { CoLaboWareData } from '@colabo-colaboware/colaboware_core/coLaboWareData';
 
 import {ColabowareRFIDService} from '@colabo-colaboware/colaboware_rfid/ColabowareRFIDService';
+
+const MAP_ID = "f7baf6923c0c84b84f0d402a";
 
 @Component({
   selector: 'app-users-profiling',
@@ -10,10 +21,45 @@ import {ColabowareRFIDService} from '@colabo-colaboware/colaboware_rfid/Colabowa
   styleUrls: ['./users-profiling.component.css']
 })
 export class UsersProfilingComponent implements OnInit {
+  edges:KEdge[] = [];
+  nodes:KNode[] = [];
 
-  constructor(private colabowareRFIDService:ColabowareRFIDService) { }
+  constructor(
+    private colabowareRFIDService: ColabowareRFIDService,
+    private knalledgeEdgeService: KnalledgeEdgeService,
+    private knalledgeNodeService: KnalledgeNodeService,
+    private knalledgeMapService: KnalledgeMapService
+  ) { }
 
   ngOnInit() {
+    this.getMapContent();
+  }
+
+  getMapContent():void{
+      //var map:KNode = new KNode();
+      // this.heroService.getHero(id)
+      //   .subscribe(hero => this.hero = hero);
+      //this.node =
+      this.knalledgeNodeService.queryInMap(MAP_ID)
+        .subscribe(nodes => this.nodesReceived(nodes)); //as KNode
+      this.knalledgeEdgeService.queryInMap(MAP_ID)
+        .subscribe(edges => this.edgesReceived(edges)); //as KNode
+  }
+
+  nodesReceived(nodesS:Array<KNode>):void{
+    // this.nodes = nodesS.data;
+    //this.nodes.fill(nodesS); //this.nodes = nodesS.data;
+    //this.nodes.name = 'test';
+    console.log('nodes: ', nodesS);
+    this.nodes = nodesS;
+  }
+
+  edgesReceived(edgesS:Array<KEdge>):void{
+    // this.edges = edgesS.data;
+    //this.edges.fill(edgesS); //this.edges = edgesS.data;
+    //this.edges.name = 'test';
+    console.log('edges: ', edgesS);
+    this.edges = edgesS;
   }
 
   profileNewUser():void
