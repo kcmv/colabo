5578375de90ccd34e92b7ba7909f23305779f122
Sasha Rudan <mprinc@gmail.com>
2018-04-09 08:13:29 -0400

    Refactored code

--- src/frontend/demos/rima/rima-colaboware/src/app/users-profiling/users-profiling.component.ts
+++ src/frontend/demos/rima/rima-colaboware/src/app/users-profiling/users-profiling.component.ts
@@ -38,7 +38,7 @@ export class UsersProfilingComponent implements OnInit {
   // global event name that is sent by @colabo-colaboware/colaboware_rfid/ColabowareRFIDService when RFID card is pressed
   colabowareIDProvided:string = "colabowareIDProvided";
 
-  @Input() new_user_name:string;
+  @Input() new_user_name:string = 'Unicorn';
   @Input() cw_data:string = '0009610521';
 
   ProfilingStateTypeNames:string[] = [
@@ -64,7 +64,6 @@ export class UsersProfilingComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.new_user_name = 'Unicorn';
 
     // load map nodes and edges
     this.getMapContent();
@@ -73,7 +72,7 @@ export class UsersProfilingComponent implements OnInit {
     // listen for RFID cards pressed
     this.globalEmitterServicesArray.register(this.colabowareIDProvided);
 
-    //this.globalEmitterServicesArray.get(this.colabowareIDProvided).subscribe('UsersProfilingComponent.user', this.createNewUser.bind(this));
+    // this.globalEmitterServicesArray.get(this.colabowareIDProvided).subscribe('UsersProfilingComponent.user', this.createNewUser.bind(this));
     this.globalEmitterServicesArray.get(this.colabowareIDProvided).subscribe('UsersProfilingComponent.user', this.colabowareInput.bind(this));
   }
 
@@ -88,76 +87,23 @@ export class UsersProfilingComponent implements OnInit {
     userNode.mapId = MAP_ID; //'56ebeabb913d88af03e9d2d6' //TODO:NG2 - use 'Demo Map' id
     userNode.name = this.new_user_name;
     userNode.type = KNode.TYPE_USER;
-    userNode.iAmId = "556760847125996dc1a4a24f";
     // later to access the RFID you would need to do:
     // let rfid = userNode.dataContent.coLaboWareData.value;
     userNode.dataContent = {
       coLaboWareData: coLaboWareData
     }
-    //TODO: iAmId, createdAt, updatedAt
-    this.knalledgeNodeService.create(userNode)
-    .subscribe(newUserCreated.bind(this));
 
-    // callback after the new user is created
-    function newUserCreated(userNode:KNode):void{
-      console.log('newUserCreated', userNode);
-      this.nodes.push(userNode);
+    // creating edge between new user and users node (with type KNode.TYPE_USERS)
+    let userEdge:KEdge = new KEdge();
+    userEdge.mapId = MAP_ID;
+    userEdge.name = "User";
+    userEdge.type = KEdge.TYPE_USER;
 
-      // creating edge between new user and node with type KNode.TYPE_USERS
-      let userEdge:KEdge = new KEdge();
-      userEdge.mapId = MAP_ID;
-      userEdge.name = "User";
-      userEdge.iAmId = "556760847125996dc1a4a24f";
-      userEdge.type = KEdge.TYPE_USER;
-      userEdge.sourceId = usersNode._id;
-      userEdge.targetId = userNode._id;
-      //TODO: iAmId, createdAt, updatedAt
-      this.knalledgeEdgeService.create(userEdge)
-      .subscribe(userEdgeCreated.bind(this));
-    }
-
-    // callback after an edge to the new user is created
-    function userEdgeCreated(userEdge:KEdge):void{
-      console.log('userEdgeCreated', userEdge);
-      this.edges.push(userEdge);
-    }
+    this.createNewNodeWithEdge(userNode, userEdge, usersNode._id, this.newUserCreated.bind(this));
   }
 
-  // get first (if there are many) node that has the provided type
-  getFirstNodeForType(type:string){
-    for(var i=0; i<this.nodes.length; i++){
-      var node = this.nodes[i];
-      if(node.type === type) return node;
-    }
-    return null;
-  }
+  newUserCreated(newNode:KNode, newEdge:KEdge){
 
-  // get map nodes and edges
-  getMapContent():void{
-      //var map:KNode = new KNode();
-      // this.heroService.getHero(id)
-      //   .subscribe(hero => this.hero = hero);
-      //this.node =
-      this.knalledgeNodeService.queryInMap(MAP_ID)
-        .subscribe(nodes => this.nodesReceived(nodes)); //as KNode
-      this.knalledgeEdgeService.queryInMap(MAP_ID)
-        .subscribe(edges => this.edgesReceived(edges)); //as KNode
-  }
-
-  nodesReceived(nodesS:Array<KNode>):void{
-    // this.nodes = nodesS.data;
-    //this.nodes.fill(nodesS); //this.nodes = nodesS.data;
-    //this.nodes.name = 'test';
-    console.log('nodes: ', nodesS);
-    this.nodes = nodesS;
-  }
-
-  edgesReceived(edgesS:Array<KEdge>):void{
-    // this.edges = edgesS.data;
-    //this.edges.fill(edgesS); //this.edges = edgesS.data;
-    //this.edges.name = 'test';
-    console.log('edges: ', edgesS);
-    this.edges = edgesS;
   }
 
   profileNewUser():void
@@ -212,6 +158,7 @@ export class UsersProfilingComponent implements OnInit {
       cwData.type = CoLaboWareType.RFID;
       cwData.value = this.cw_data;
       this.colabowareInput(cwData);
+      // this.createNewUser(cwData);
   }
 
   rfidEnable(){
@@ -238,5 +185,73 @@ export class UsersProfilingComponent implements OnInit {
           break;
       }
     }
+  }
+
+  /** Code for dealing with maps, probably repetition from KnAllEdge code :(
+  */
+
+  createNewNodeWithEdge(newNode:KNode, newEdge:KEdge, parentNodeId:string, listener){
+    newNode.iAmId = "556760847125996dc1a4a24f";
+    newEdge.iAmId = "556760847125996dc1a4a24f";
+
+    //TODO: iAmId, createdAt, updatedAt
+    this.knalledgeNodeService.create(newNode)
+    .subscribe(newNodeCreated.bind(this));
+
+    // callback after the new user is created
+    function newNodeCreated(newNode:KNode):void{
+      console.log('newUserCreated', newNode);
+      this.nodes.push(newNode);
+
+      newEdge.sourceId = parentNodeId;
+      newEdge.targetId = newNode._id;
+      //TODO: iAmId, createdAt, updatedAt
+      this.knalledgeEdgeService.create(newEdge)
+      .subscribe(newEdgeCreated.bind(this));
+
+      // callback after an edge to the new node is created
+      function newEdgeCreated(newEdge:KEdge):void{
+        console.log('newEdgeCreated', newEdge);
+        listener(newNode, newEdge);
+        this.edges.push(newEdge);
+      }
+    }
+  }
+
+  // get first (if there are many) node that has the provided type
+  getFirstNodeForType(type:string){
+    for(var i=0; i<this.nodes.length; i++){
+      var node = this.nodes[i];
+      if(node.type === type) return node;
+    }
+    return null;
+  }
+
+  // get map nodes and edges
+  getMapContent():void{
+      //var map:KNode = new KNode();
+      // this.heroService.getHero(id)
+      //   .subscribe(hero => this.hero = hero);
+      //this.node =
+      this.knalledgeNodeService.queryInMap(MAP_ID)
+        .subscribe(nodes => this.nodesReceived(nodes)); //as KNode
+      this.knalledgeEdgeService.queryInMap(MAP_ID)
+        .subscribe(edges => this.edgesReceived(edges)); //as KNode
+  }
+
+  nodesReceived(nodesS:Array<KNode>):void{
+    // this.nodes = nodesS.data;
+    //this.nodes.fill(nodesS); //this.nodes = nodesS.data;
+    //this.nodes.name = 'test';
+    console.log('nodes: ', nodesS);
+    this.nodes = nodesS;
+  }
+
+  edgesReceived(edgesS:Array<KEdge>):void{
+    // this.edges = edgesS.data;
+    //this.edges.fill(edgesS); //this.edges = edgesS.data;
+    //this.edges.name = 'test';
+    console.log('edges: ', edgesS);
+    this.edges = edgesS;
   }
 }
