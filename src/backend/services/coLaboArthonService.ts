5eea469c23835cb2c052a6b33280b0a83e278676
Sinisa Rudan <sinisa.rudan@gmail.com>
2018-05-10 13:48:14 +0200

    small changes

--- src/backend/services/coLaboArthonService.ts
+++ src/backend/services/coLaboArthonService.ts
@@ -5,6 +5,14 @@ const MAP_ID:string = "5af39ce82843ddf04b459cb0";
 
 //export public class CoLaboArthonService {
 export class CoLaboArthonService {
+
+  //TODO: maxiId
+  //TODO create in DB node for replies
+
+  getNodeByHumanID():KNode{
+    return null;
+  }
+
   saveParticipant(name:string, background:string){
     //this.createNewUser();
   }
