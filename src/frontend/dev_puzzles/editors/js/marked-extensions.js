f3275dd578ee5c3cd5c646f3065a7171b1537426
Sasha Rudan <mprinc@gmail.com>
2018-08-12 22:49:45 +0100

    reformating

--- src/frontend/dev_puzzles/editors/js/marked-extensions.js
+++ src/frontend/dev_puzzles/editors/js/marked-extensions.js
@@ -1,6 +1,6 @@
 // setting up marked
 marked.setOptions({
-  gfm: true // this should be true by default
+    gfm: true // this should be true by default
 });
 
 // https://www.npmjs.com/package/marked#renderer
@@ -10,11 +10,11 @@ var renderer = new marked.Renderer();
 var originalLink = renderer.link.bind(renderer);
 // open a link in a new tab
 renderer.link = function link(href, title, text) {
-  var linkStr = originalLink(href, title, text);
-  linkStr = linkStr.replace("<a", '<a target="_blank"');
-  return linkStr;
+    var linkStr = originalLink(href, title, text);
+    linkStr = linkStr.replace("<a", '<a target="_blank"');
+    return linkStr;
 }
 
 marked.nodeEditor = {
-  renderer: renderer
+    renderer: renderer
 };
