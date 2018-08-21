40301565d971d881ab815707cd5d709a1d85b1ae
Sasha Rudan <mprinc@gmail.com>
2018-08-21 03:42:48 +0100

    Updated frontend colabo config file

--- src/frontend/colabo.config.js
+++ src/frontend/colabo.config.js
@@ -1,40 +1,37 @@
 var puzzles = {
-  dependencies: {
-    "@colabo-puzzles/puzzles_core": {
+    name: "f-colabo.space",
+    description: "Colabo.space ecosystem - frontend",
+    dependencies: {
+        "@colabo-puzzles/puzzles_core": {},
+        "@colabo-knalledge/knalledge_core": {},
+        "@colabo-knalledge/knalledge_store_core": {},
+        "@colabo-knalledge/knalledge_view_enginee": {},
+        "@colabo-knalledge/knalledge_view_interaction": {}
     },
-    "@colabo-knalledge/knalledge_core": {
-    },
-    "@colabo-knalledge/knalledge_store_core": {
-    },
-    "@colabo-knalledge/knalledge_view_enginee": {
-    },
-    "@colabo-knalledge/knalledge_view_interaction": {
+    offers: {
+        "@colabo-puzzles/puzzles_core": {
+            npm: "@colabo-puzzles/puzzles_core",
+            path: "dev_puzzles/puzzles/puzzles_core"
+        },
+        "@colabo-knalledge/knalledge_core": {
+            npm: "@colabo-knalledge/knalledge_core",
+            path: "dev_puzzles/knalledge/knalledge_core"
+        },
+        "@colabo-knalledge/knalledge_store_core": {
+            npm: "@colabo-knalledge/knalledge_store_core",
+            path: "dev_puzzles/knalledge/knalledge_store_core"
+        },
+        "@colabo-knalledge/knalledge_search": {
+            npm: "@colabo-knalledge/knalledge_search",
+            path: "dev_puzzles/knalledge/knalledge_search"
+        },
+        "@colabo-knalledge/knalledge_view_enginee": {
+            npm: "@colabo-knalledge/knalledge_view_enginee",
+            path: "dev_puzzles/knalledge/knalledge_view_enginee"
+        },
+        "@colabo-knalledge/knalledge_view_interaction": {
+            npm: "@colabo-knalledge/knalledge_view_interaction",
+            path: "dev_puzzles/knalledge/knalledge_view_interaction"
+        }
     }
-  },
-  offers: {
-    "@colabo-puzzles/puzzles_core": {
-      npm: "@colabo-puzzles/puzzles_core",
-      path: "dev_puzzles/puzzles/puzzles_core"
-    },
-    "@colabo-knalledge/knalledge_core": {
-      npm: "@colabo-knalledge/knalledge_core",
-      path: "dev_puzzles/knalledge/knalledge_core"
-    },
-    "@colabo-knalledge/knalledge_store_core": {
-      npm: "@colabo-knalledge/knalledge_store_core",
-      path: "dev_puzzles/knalledge/knalledge_store_core"
-    },
-    "@colabo-knalledge/knalledge_search": {
-      npm: "@colabo-knalledge/knalledge_search",
-      path: "dev_puzzles/knalledge/knalledge_search"
-    },
-    "@colabo-knalledge/knalledge_view_enginee": {
-      npm: "@colabo-knalledge/knalledge_view_enginee",
-      path: "dev_puzzles/knalledge/knalledge_view_enginee"
-    },
-    "@colabo-knalledge/knalledge_view_interaction": {
-      npm: "@colabo-knalledge/knalledge_view_interaction",
-      path: "dev_puzzles/knalledge/knalledge_view_interaction"
-    }
-  }
 }
