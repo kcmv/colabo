8d365f1179d25ed47eae6172b955e7fc4f0f118e
Sinisa Rudan <sinisa.rudan@gmail.com>
2018-09-08 17:56:51 +0200

    dialogame card user img supported - anonymous

--- src/frontend/apps/nca/src/app/dialo-game/dialogame-cards/dialogame-card/dialogame-card.component.ts
+++ src/frontend/apps/nca/src/app/dialo-game/dialogame-cards/dialogame-card/dialogame-card.component.ts
@@ -9,10 +9,20 @@ import {KNode} from '@colabo-knalledge/knalledge_core/code/knalledge/kNode';
 export class DialogameCardComponent implements OnInit {
   @Input() cardData:KNode;
 
+  userIconsPath:string = 'assets/images/user_icons/';
+
   constructor() { }
 
   ngOnInit() {
     console.log("cardData", this.cardData);
+  }
+
+  getUserName():string{
+    return 'unknown user';
+  }
+
+  getIconImg():string{
+    return 'performer.jpg';
   }
 
 }
