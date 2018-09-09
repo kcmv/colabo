52c558c7cb438eafe5462a2e3d0e2aa4379e76e2
Sasha Rudan <mprinc@gmail.com>
2018-09-09 01:21:35 +0100

    Moved knalledge providers inside the puzzle module

--- src/frontend/demos/knalledge/knalledge-view-node/src/app/app.module.ts
+++ src/frontend/demos/knalledge/knalledge-view-node/src/app/app.module.ts
@@ -12,25 +12,20 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import 'hammerjs';
 import {MaterialModule} from './materialModule';
 
-import {KnalledgeEdgeService} from '@colabo-knalledge/knalledge_store_core/knalledge-edge.service';
-import {KnalledgeNodeService} from '@colabo-knalledge/knalledge_store_core/knalledge-node.service';
-import {KnalledgeMapService} from '@colabo-knalledge/knalledge_store_core/knalledge-map.service';
-import { KnalledgeSearchNodeService } from '@colabo-knalledge/knalledge_search/knalledge-search-nodes.service';
-
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { GetMapComponent } from './get-map/get-map.component';
 
-// Puzzle Modules
-import { KnaledgeViewNodeModule } from '@colabo-knalledge/knalledge_view_node/module';
-import { RimaAaaModule } from '@colabo-rima/rima_aaa/module';
-
 var moduleDeclarations = [
   AppComponent,
   GetMapComponent,
 ];
+
+// Puzzle Modules
+import { KnaledgeViewNodeModule } from '@colabo-knalledge/knalledge_view_node/module';
+import { RimaAaaModule } from '@colabo-rima/rima_aaa/module';
 
 var moduleImports = [
   BrowserModule,
@@ -64,7 +59,6 @@ export var Plugins:any = window.Config.Plugins;
   imports: moduleImports,
   entryComponents: [],
   providers: [
-    KnalledgeEdgeService, KnalledgeNodeService, KnalledgeMapService, KnalledgeSearchNodeService,
     {provide: "Plugins", useValue: Plugins},
     // provide ng build error: "Can't resolve all parameters for GlobalEmitterService"
     // {provide: GlobalEmitterService, useClass: GlobalEmitterService},
