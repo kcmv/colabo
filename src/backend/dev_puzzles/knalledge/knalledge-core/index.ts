ba3012fb749cb946122374005542572ca789ea92
Sasha Rudan <mprinc@gmail.com>
2018-09-16 06:05:01 +0100

    correction

--- src/backend/dev_puzzles/knalledge/knalledge-core/index.ts
+++ src/backend/dev_puzzles/knalledge/knalledge-core/index.ts
@@ -1,9 +1,9 @@
 export function KnAllEdgeCoreRegister(app) {
     console.log("[puzzle(knalledge/core) - /models/index.js] Registering KnAllEdge core API to: ", app);
 
-    var knodes = app.resource('knodes', require('./modules/kNode'), { id: 'type?/:actionType?/:searchParam?/:searchParam2?' });
-    var kedges = app.resource('kedges', require('./modules/kEdge'), { id: 'type?/:searchParam?/:searchParam2?' });
-    var kmaps = app.resource('kmaps', require('./modules/kMap'), { id: 'type?/:searchParam?/:searchParam2?' });
+    let knodes = app.resource('knodes', require('./modules/kNode'), { id: 'type?/:actionType?/:searchParam?/:searchParam2?' });
+    let kedges = app.resource('kedges', require('./modules/kEdge'), { id: 'type?/:searchParam?/:searchParam2?' });
+    let kmaps = app.resource('kmaps', require('./modules/kMap'), { id: 'type?/:searchParam?/:searchParam2?' });
 }
 
 import {VO} from './lib/VO';
