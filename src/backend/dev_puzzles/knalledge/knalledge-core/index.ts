3f7482332697cfa798d5f2e4fd2c1796af98d0ef
Sasha Rudan <mprinc@gmail.com>
2018-09-17 21:46:01 +0100

    Fixed bckend building process

--- src/backend/dev_puzzles/knalledge/knalledge-core/index.ts
+++ src/backend/dev_puzzles/knalledge/knalledge-core/index.ts
@@ -1,9 +1,9 @@
 export function KnAllEdgeCoreRegister(app) {
     console.log("[puzzle(knalledge/core) - /models/index.js] Registering KnAllEdge core API to: ", app);
 
-    var knodes = app.resource('knodes', require('./modules/kNode'), { id: 'type?/:actionType?/:searchParam?/:searchParam2?' });
-    var kedges = app.resource('kedges', require('./modules/kEdge'), { id: 'type?/:searchParam?/:searchParam2?' });
-    var kmaps = app.resource('kmaps', require('./modules/kMap'), { id: 'type?/:searchParam?/:searchParam2?' });
+    var knodes = app.resource('knodes', require('./lib/modules/kNode'), { id: 'type?/:actionType?/:searchParam?/:searchParam2?' });
+    var kedges = app.resource('kedges', require('./lib/modules/kEdge'), { id: 'type?/:searchParam?/:searchParam2?' });
+    var kmaps = app.resource('kmaps', require('./lib/modules/kMap'), { id: 'type?/:searchParam?/:searchParam2?' });
 }
 
 import {VO} from './lib/VO';
