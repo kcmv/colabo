cb5de438b6fa2289b8634d53b32c1b88d7bff945
Sinisa Rudan <sinisa.rudan@gmail.com>
2018-11-07 19:46:10 +0100

    TopicChat for CoLaboFlow changes

--- src/frontend/dev_puzzles/flow/core/lib/moderation/colabo-flow-m.component.ts
+++ src/frontend/dev_puzzles/flow/core/lib/moderation/colabo-flow-m.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {ColaboFlowService} from '../colabo-flow.service';
+import {ColaboFlowService, ColaboFlowEvents, TopiChatPackage, ColaboPubSubPlugin} from '../colabo-flow.service';
 import {ColaboFlowState} from '../colaboFlowState';
 import {MyColaboFlowState} from '../myColaboFlowState';
+import {RimaAAAService} from '@colabo-rima/f-aaa';
+import {KNode} from '@colabo-knalledge/f-core/code/knalledge/kNode';
 
 @Component({
   selector: 'colabo-flow-m',
@@ -11,15 +13,51 @@ import {MyColaboFlowState} from '../myColaboFlowState';
 export class ColaboFlowMComponent implements OnInit {
 
   constructor(
-    private colaboFlowService: ColaboFlowService
+    private colaboFlowService: ColaboFlowService,
+    protected rimaAAAService:RimaAAAService
   ) { }
 
   ngOnInit() {
+    // registering system plugin
+    let colaboFlowOptions:ColaboPubSubPlugin = {
+        name: "colaboFlow",
+        events: {}
+    };
+    colaboFlowOptions.events[ColaboFlowEvents.ColaboFlowStateChange] = this.colaboFlowStateChanged.bind(this);
+    this.colaboFlowService.registerPlugin(colaboFlowOptions);
+  }
+
+  colaboFlowStateChanged(eventName, msg, tcPackage:TopiChatPackage):void {
+      console.log('[colaboFlowStateChanged] Client id: %s', tcPackage.clientIdReciever);
+      console.log('\t msg: %s', JSON.stringify(tcPackage.msg));
   }
 
   nextState():void{
     this.colaboFlowService.colaboFlowState.nextState();
     this.colaboFlowService.saveCFState();
+    this.sendMessage(this.colaboFlowService.colaboFlowState);
+  }
+
+  sendMessage(colaboFlowState:ColaboFlowState){
+      let whoAmI:KNode = this.rimaAAAService.getUser();
+
+      var msg:any = {
+        meta: {
+          timestamp: Math.floor(new Date().getTime() / 1000)
+        },
+        from: {
+          name: whoAmI.name, // whoAmI.dataContent.firstName
+          role: 'moderator',
+          iAmId: whoAmI._id
+        },
+        content: {
+          colaboFlowState: colaboFlowState
+        }
+      };
+      console.log('[ColaboFlowMComponent:sendMessage] sending message: %s', colaboFlowState);
+      this.colaboFlowService.emit(ColaboFlowEvents.ColaboFlowStateChange, msg);
+      // this.messages.push(msg);
+      // this.messageContent = "";
   }
 
   previousState():void{
