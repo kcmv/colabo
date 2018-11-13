38202e948fcf5f449bc5b5ff27c6a2318d2e8d60
Sasha Rudan <mprinc@gmail.com>
2018-11-13 03:51:28 +0100

    Updated avatar

--- src/frontend/dev_puzzles/media/upload/lib/avatar/avatar.component.ts
+++ src/frontend/dev_puzzles/media/upload/lib/avatar/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RimaAAAService } from '@colabo-rima/f-aaa/rima-aaa.service';
 import {UserData} from '@colabo-rima/f-aaa/userData';
 import { KNode } from '@colabo-knalledge/f-core/code/knalledge/kNode';
@@ -10,6 +10,10 @@ import { MediaUploadService } from '../upload.service';
   styleUrls: ['./avatar.component.css']
 })
 export class AvatarComponent implements OnInit {
+  @ViewChild('file') file;
+  public uploadStatus:string = 'avatar not sent';
+  public fileAdded:boolean = false;
+  public fileUploaded:boolean = false;
 
   constructor(
     protected mediaUploadService: MediaUploadService,
@@ -28,21 +32,42 @@ export class AvatarComponent implements OnInit {
     return this.RimaAAAService.getUser();
   }
 
+  getPhoto(){
+    this.file.nativeElement.click();
+  }
+
   onFileAdded(event){
-    let fileList: FileList = event.target.files;
+    this.fileAdded = true;
+
+    // let fileList: FileList = event.target.files;
+    // this.mediaUploadService.uploadFileList(fileList).subscribe(val => {
+    //   // if (val.json().status == 'success') {
+    //   //   this.networkContract.FilePath = val.json().data.fileName;
+    //   // }
+    //   // console.log(val.json());
+    // });
+  }
+
+  onSubmit( ){
+    // console.log(this.form);
+    // let userData:UserData = new UserData();
+    //TODO: userData.image.url = this.form.value.cameraPhoto;
+    // this.RimaAAAService.createNewUser(userData, this.userCreated);
+    let fileList: FileList = this.file.nativeElement.files;
+    this.uploadStatus = 'avatar is being sent';
     this.mediaUploadService.uploadFileList(fileList).subscribe(val => {
       // if (val.json().status == 'success') {
       //   this.networkContract.FilePath = val.json().data.fileName;
       // }
       // console.log(val.json());
+      this.uploadStatus = 'is sent';
+      this.fileUploaded = true;
     });
   }
-
-  onSubmit( ){
-    // console.log(this.form);
-    let userData:UserData = new UserData();
-    //TODO: userData.image.url = this.form.value.cameraPhoto;
-    // this.RimaAAAService.createNewUser(userData, this.userCreated);
+  
+  reset(){
+    this.uploadStatus = 'avatar not sent';
+    this.fileAdded = false;
+    this.fileUploaded = false;
   }
-
 }
