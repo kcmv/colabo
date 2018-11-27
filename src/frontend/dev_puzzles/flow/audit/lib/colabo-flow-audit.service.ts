d24101b66f9251d19f7337a55b6f81d9199de18c
Sasha Rudan <mprinc@gmail.com>
2018-11-27 08:49:03 +0000

    Added concrete audit actions

--- src/frontend/dev_puzzles/flow/audit/lib/colabo-flow-audit.service.ts
+++ src/frontend/dev_puzzles/flow/audit/lib/colabo-flow-audit.service.ts
@@ -30,17 +30,84 @@ export class ColaboFlowAuditService{
   getItems():AuditedAction[]{
     let items:AuditedAction[] = [];
     items.push(({
-      _id: "ff3c",
-      name: "Сава"
+      _id: "ad30",
+      name: "start",
+      flowId: "searchSoundsNoCache",
+      flowInstanceId: "ff01"
     }) as AuditedAction);
+
     items.push(({
-      _id: "ad3c",
-      name: "Николај"
+      _id: "ad31",
+      name: "parseRequest",
+      flowId: "searchSoundsNoCache",
+      flowInstanceId: "ff01"
     }) as AuditedAction);
+
     items.push(({
-      _id: "872е",
-      name: "Симеон"
+      _id: "ad32",
+      name: "parseResponse",
+      flowId: "searchSoundsNoCache",
+      flowInstanceId: "ff01"
+    }) as AuditedAction);
+
+    items.push(({
+      _id: "ad3a",
+      name: "end",
+      flowId: "searchSoundsNoCache",
+      flowInstanceId: "ff01"
+    }) as AuditedAction);
+
+    // flow ff02
+    items.push(({
+      _id: "ad40",
+      name: "start",
+      flowId: "searchSoundsNoCache",
+      flowInstanceId: "ff02"
+    }) as AuditedAction);
+
+    items.push(({
+      _id: "ad41",
+      name: "parseRequest",
+      flowId: "searchSoundsNoCache",
+      flowInstanceId: "ff02"
+    }) as AuditedAction);
+
+    items.push(({
+      _id: "ad42",
+      name: "parseResponse",
+      flowId: "searchSoundsNoCache",
+      flowInstanceId: "ff02"
+    }) as AuditedAction);
+
+    items.push(({
+      _id: "ad4a",
+      name: "end",
+      flowId: "searchSoundsNoCache",
+      flowInstanceId: "ff02"
+    }) as AuditedAction);
+
+    // flow ff03
+    items.push(({
+      _id: "ad50",
+      name: "start",
+      flowId: "searchSoundsWithCache",
+      flowInstanceId: "ff03"
+    }) as AuditedAction);
+
+    items.push(({
+      _id: "ad50",
+      name: "parseRequest",
+      flowId: "searchSoundsWithCache",
+      flowInstanceId: "ff03"
     }) as AuditedAction);
     return items;
+
+    items.push(({
+      _id: "ad5a",
+      name: "end",
+      flowId: "searchSoundsWithCache",
+      flowInstanceId: "ff03"
+    }) as AuditedAction);
+
   }
 }
