5e84f8f7d52930168087da7f1bae05f3435a5989
Sinisa Rudan <sinisa.rudan@gmail.com>
2018-11-30 21:01:22 +0100

    paricipant circle - displayed real participants images; added small normalized avatars

--- src/frontend/dev_puzzles/moderation/core/moderation-panel/insights/results-visualization/participants-circle/participants-circle.ts
+++ src/frontend/dev_puzzles/moderation/core/moderation-panel/insights/results-visualization/participants-circle/participants-circle.ts
@@ -9,7 +9,7 @@ declare let d3:any;
 declare let bb:any;
 // import {bb} from 'billboard';
 
-const UseSVG:boolean=false;
+const Radius:number = 250;
 
 @Component({
   selector: 'participants-circle',
@@ -17,7 +17,7 @@ const UseSVG:boolean=false;
   styleUrls: ['./participants-circle.css']
 })
 export class ParticipantsCircleComponent implements OnInit {
-
+  nodes:any[] = [];
   // title = 'app';
   // color:string = 'blue';
   // radius = 10;
@@ -46,46 +46,64 @@ export class ParticipantsCircleComponent implements OnInit {
   private createCanvas(radius:number):any {
     // d3.selectAll('svg').remove();
     var canvas:any = d3.select('#participants-circle-canvas');
-    if(UseSVG){
-      canvas = canvas.append('svg:svg')
-        .attr('width', (radius * 2) + 150)
-        .attr('height', (radius * 2) + 150);
-    }
-    else{
-      //TODO: not working - not stretching the parent DIV
-      canvas
-        .attr('width', (radius * 2) + 150 + 'px')
-        .attr('height', (radius * 2) + 150 + 'px');
-    }
+    
+    //TODO: not working - not stretching the parent DIV
+    canvas
+      .attr('width', (radius * 2) + 150 + 'px')
+      .attr('height', (radius * 2) + 150 + 'px');
+  
     return canvas;
   }
 
-  private createNodes(radius:number, users:KNode[]):any[] {
-    let nodes:any[] = [], 
-        width:number = (radius * 2) + 50,
-        height:number = (radius * 2) + 50,
+  private checkImageExists(imageUrl:string, callBack:Function):void {
+    var imageData = new Image();
+    imageData.onload = function() {
+      callBack(true);
+    };
+    imageData.onerror = function() {
+      console.log('NON EXISTING IMG: '+imageUrl);
+      // imageUrl = 'https://fv.colabo.space/assets/images/user_icons/performer.jpg';
+      callBack(false);
+    };
+    imageData.src = imageUrl;
+    }
+
+  private createNodes(users:KNode[]):void {
+    let that:ParticipantsCircleComponent = this;
+    let width:number = (Radius * 2) + 50,
+        height:number = (Radius * 2) + 50,
         angle:number,
         x:number,
         y:number,
         i:number;
     let numNodes:number = users.length;
+    let imgLoadsLeft:number = numNodes;
     for (i=0; i<numNodes; i++) {
      angle = (i / (numNodes/2)) * Math.PI; // Calculate the angle at which the element will be placed.
                                            // For a semicircle, we would use (i / numNodes) * Math.PI.
-     x = (radius * Math.cos(angle)) + (width/2); // Calculate the x position of the element.
-     y = (radius * Math.sin(angle)) + (height/2); // Calculate the y position of the element.
-     //TODO: temporary:
-     users[i].dataContent.avatar = 'https://fv.colabo.space/assets/images/user_icons/performer.jpg';
-     nodes.push({'id': i, 'x': x, 'y': y, 'user':users[i]});
+     x = (Radius * Math.cos(angle)) + (width/2); // Calculate the x position of the element.
+     y = (Radius * Math.sin(angle)) + (height/2); // Calculate the y position of the element.
+     let dataContent = users[i].dataContent;
+     dataContent.avatar = 'https://fv.colabo.space/assets/images/avatars/user.avatar-' + users[i]._id + '.jpg';
+     this.checkImageExists(dataContent.avatar, 
+      function(result:boolean){
+        imgLoadsLeft--;
+        if(!result){
+          dataContent.avatar = 'https://fv.colabo.space/assets/images/user_icons/performer.jpg';
+        }
+        if(imgLoadsLeft===0){
+          that.nodesCreated();
+        }
+    });
+     this.nodes.push({'id': i, 'x': x, 'y': y, 'user':users[i]});
     }
-    return nodes;
   }
 
-  private createElements(canvas:any, nodes:any[], elementRadius:number, tooltip:any):void{
-    console.log('[createElements] nodes', nodes);
+  private createElements(canvas:any, elementRadius:number, tooltip:any):void{
+    console.log('[createElements] nodes', this.nodes);
     const ParticipantOpacityStart:number = 0.6;
     let element = canvas.selectAll('circle')
-      .data(nodes)
+      .data(this.nodes)
       .enter()
         // .append('canvas:circle')
         // .attr('r', elementRadius)
@@ -95,29 +113,17 @@ export class ParticipantsCircleComponent implements OnInit {
         // .attr('cy', function (d, i) {
         //   return d.y;
         // })
-        .append(UseSVG ? "svg:image" : 'img')
-        .attr(UseSVG ? "xlink:href" : 'src',  function(d) { return d.user.dataContent.avatar;})
+        .append('img')
+        .attr('src',  function(d) { return d.user.dataContent.avatar;})
         .attr("height", 50)
         .attr("width", 50)
-
-        if(UseSVG){
-          element
-          .attr("x", function (d, i) {
-            return d.x;
-          })
-          .attr("y", function (d, i) {
-            return d.y;
-          })
-        }else{
-          element
-          .style('left', function (d, i) {
-            return d.x + 'px';
-          })
-          .style('top', function (d, i) {
-            return d.y + 'px';
-          })
-          .style("position", 'fixed')
-        }
+        .style('left', function (d, i) {
+          return d.x + 'px';
+        })
+        .style('top', function (d, i) {
+          return d.y + 'px';
+        })
+        .style("position", 'fixed')
 
         element
         //class adding doesn't works:
@@ -135,8 +141,8 @@ export class ParticipantsCircleComponent implements OnInit {
           d3.select( this )
           .raise() //putting it visually in front of the other participants
           .transition()
-          .attr(UseSVG ? "x" : 'left', function(d) { return d.x-30;})
-          .attr(UseSVG ? "y" : 'top', function(d) { return d.y-30;})
+          .attr('left', function(d) { return d.x-30;})
+          .attr('top', function(d) { return d.y-30;})
           .attr("height", 80)
           .attr("width", 80)
           .style("opacity", 1);	
@@ -155,8 +161,8 @@ export class ParticipantsCircleComponent implements OnInit {
 
         d3.select( this )
           .transition()
-          .attr(UseSVG ? "x" : 'left', function(d) { return d.x;})
-          .attr(UseSVG ? "y" : 'top', function(d) { return d.y;})
+          .attr('left', function(d) { return d.x;})
+          .attr('top', function(d) { return d.y;})
           .attr("height", 50)
           .attr("width", 50)
           .style("opacity", ParticipantOpacityStart);	
@@ -169,11 +175,13 @@ export class ParticipantsCircleComponent implements OnInit {
 
   participantsCircle(users:KNode[]):void{
     let numNodes:number = users.length;
-    let radius:number = 250;
-    let elementRadius:number = 15;
-    let nodes = this.createNodes(radius, users);
-    let canvas = this.createCanvas(radius);
+    
+    this.createNodes(users);
+  }
 
+  nodesCreated():void{
+    let canvas = this.createCanvas(Radius);
+    let elementRadius:number = 15;
     // Define the div for the tooltip
     let tooltip = d3.select("#participants-circle-tooltip")
     .style("opacity", 0);
@@ -186,7 +194,8 @@ export class ParticipantsCircleComponent implements OnInit {
     // tooltip.html('tt-user')
     //   .style("left", "30px")		
     //   .style("top", "50px");	
-    this.createElements(canvas, nodes, elementRadius, tooltip);
+    
+    this.createElements(canvas, elementRadius, tooltip);
   }
   
 
