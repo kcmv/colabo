b1e93d971bd8caa2c1b440fb64913d384582fa9c
Sinisa Rudan <sinisa.rudan@gmail.com>
2019-01-05 19:49:44 +0100

    testing fixes - not finished

--- src/frontend/apps/psc/src/app/dialo-game/dialogame-cards/dialogame-cards.component.spec.ts
+++ src/frontend/apps/psc/src/app/dialo-game/dialogame-cards/dialogame-cards.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { InteractionCardsComponent } from './interaction-cards.component';
+import { DialogameCardsComponent } from './dialogame-cards.component';
 
-describe('InteractionCardsComponent', () => {
-  let component: InteractionCardsComponent;
-  let fixture: ComponentFixture<InteractionCardsComponent>;
+describe('DialogameCardsComponent', () => {
+  let component: DialogameCardsComponent;
+  let fixture: ComponentFixture<DialogameCardsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ InteractionCardsComponent ]
+      declarations: [ DialogameCardsComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(InteractionCardsComponent);
+    fixture = TestBed.createComponent(DialogameCardsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/frontend/dev_puzzles/flow/core/index.ts
+++ src/frontend/dev_puzzles/flow/core/index.ts
@@ -6,5 +6,6 @@ export {ColaboFlowCoreModule} from './lib/module';
 export {ColaboFlowComponent} from './lib/info/colabo-flow.component';
 export {ColaboFlowMComponent} from './lib/moderation/colabo-flow-m.component';
 export {ColaboFlowService} from './lib/colabo-flow.service';
+// export {MyColaboFlowState} from './lib/myColaboFlowState';
 // export {TopiChatCoreService, TopiChatPackage, ColaboPubSubPlugin} from './lib/topiChat-core.service';
 // export {TopiChatSimpleMessageForm} from './lib/simple-message-form/simple-message-form.component';
