b4dab327a318c6a3e03fc5f8920916ad8c6dc477
Sasha Rudan <mprinc@gmail.com>
2019-06-20 00:11:39 +0100

    Updated yarn regarding checksum-changed colabo downloadable packages

--- src/isomorphic/dev_puzzles/flow/go/index.ts
+++ src/isomorphic/dev_puzzles/flow/go/index.ts
@@ -2,4 +2,4 @@
  * Public API Surface of `@colabo-flow/i-go`
  */
 
-export { RpcMethods, RpcCallback, RpcMethodSingle, BpmnSymbolType, EventType, EventSubType, ActivityType, GatewayType, BpmnSymbolSubType, BpmnSymbolSubSubType, ActionExecuteRequest, ActionExecuteRequestClass, ActionExecuteRequestAny, ActionExecuteReply } from './lib/colabo-flow-vos';
+export { RpcMethods, RpcCallback, RpcMethodSingle, BpmnSymbolType, EventType, EventSubType, ActivityType, GatewayType, BpmnSymbolSubType, BpmnSymbolSubSubType, ActionExecuteRequestInterface, ActionExecuteRequestClass, ActionExecuteRequestAny, ActionExecuteReply } from './lib/colabo-flow-vos';
--- src/isomorphic/dev_puzzles/flow/go/lib/colabo-flow-vos.ts
+++ src/isomorphic/dev_puzzles/flow/go/lib/colabo-flow-vos.ts
@@ -60,78 +60,46 @@ export enum ActionExecuteRequestUnknownDefaults {
 
 // we introduce this one so we can 
 export interface ActionExecuteRequestAny {
-    id?: any;
-    time?: any;
-
-    // action types
-    bpmn_type: any;
-    bpmn_subtype: any;
-    bpmn_subsubtype?: any;
-
-    flowId: any;
+    flowId?: any;
     name: any;
-
-    userId?: any;
-    sessionId?: any;
     flowInstanceId: any;
 
-    implementationId?: any;
+    dataIn?: any;
+    params?: any;
+
+    implementationId?:any;
     implementerId?: any;
-    success: any;
 }
 
-export interface ActionExecuteRequest{
-    id?: string; // unique id of the audited action
-    time?: string; // when did the action happen
-    dataIn?: string;
-
-    // action types
-    bpmn_type: BpmnSymbolType; // action type (BpmnSymbolType.Event, ...)
-    bpmn_subtype: BpmnSymbolSubType; // sub type (ActivityType.Task, EventType.Start, ...)
-    bpmn_subsubtype?: BpmnSymbolSubSubType; // sub-sub type (EventSubType.Message, ...)
-
-    flowId: string; // name of the flow the action belongs to
+export interface ActionExecuteRequestInterface{
+    flowId?: string; // name of the flow the action belongs to
     name: string; // name of the action (inside the flow)
-
-    userId?: string; // id of user initiating the action
-    sessionId?: string; // id of the session under which the call is initiated
     flowInstanceId: string; // id of the flow instance that started
+    
+    dataIn?: string;
+    params?: string;
 
-    implementationId?: string; // id of the implementation used for performing the action (i.e. if it is a person performing it, (s)he has been using a tool implemented and identified with implementationID) ('oven Smederevka')
-    implementerId?: string; // id of the person or component performing the action ('baker', or 'oven')
-    success: boolean;
+    implementationId?:string; // id of the implementation used for performing the action (i.e. if it is a person performing it, (s)he has been using a tool implemented and identified with implementationID) ('oven Smederevka')
+    implementerId?:string; // id of the person or component performing the action ('baker', or 'oven')
 }
 
-export class ActionExecuteRequestClass implements ActionExecuteRequest, ActionExecuteRequestAny {
-    id: string;
-    time: string;
+export class ActionExecuteRequestClass implements ActionExecuteRequestInterface, ActionExecuteRequestAny {
+    flowId?: string; 
+    name: string; 
+    flowInstanceId: string; 
 
-    // action types
-    bpmn_type: BpmnSymbolType;
-    bpmn_subtype: BpmnSymbolSubType;
-    bpmn_subsubtype: BpmnSymbolSubSubType;
+    dataIn?: string;
+    params?: string;
 
-    flowId: string;
-    name: string;
-
-    userId: string;
-    sessionId: string;
-    flowInstanceId: string;
-
-    implementationId: string;
-    implementerId: string;
-    success: boolean;
-    
-    // coming from IPluginAuditing
-    createdAt: string;
-    updatedAt: string;
+    implementationId?: string;
+    implementerId?: string; 
     
     constructor(){
         
     }
     
     init(){
-        
+
     }
 }
 
@@ -139,4 +107,5 @@ export interface ActionExecuteReply {
     id: string; // unique id of the executed action
     dataOut: string; // serialized data output of the action
     params: string; // serialized parameters of the action
+    error: string;
 }
