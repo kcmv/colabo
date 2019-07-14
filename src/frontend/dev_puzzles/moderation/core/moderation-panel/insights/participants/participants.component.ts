5545744147d749937da4e2d69e938042a59e76e6
Sinisa Rudan <sinisa.rudan@gmail.com>
2019-07-14 15:35:36 +0200

    pariticipants display progressed: photo,  bio, loadin from json file, etc

--- src/frontend/dev_puzzles/moderation/core/moderation-panel/insights/participants/participants.component.ts
+++ src/frontend/dev_puzzles/moderation/core/moderation-panel/insights/participants/participants.component.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http'; //for loading participants file
+import { Observable } from 'rxjs';
 import { Component, OnInit, Input } from '@angular/core';
 import {KNode} from '@colabo-knalledge/f-core/code/knalledge/kNode';
 import {KEdge} from '@colabo-knalledge/f-core/code/knalledge/kEdge';
@@ -15,6 +17,9 @@ export class ParticipantsComponent implements OnInit {
   }
    private _participants:any[] = []; //KNode
 
+   constructor(private http: HttpClient) {
+  }
+
   // @Input() set userData(ud:KNode){
   //   this._userData = ud;
   //   this.getSDGSelections();
@@ -29,21 +34,35 @@ export class ParticipantsComponent implements OnInit {
   }
   get userOptions():any{
     return {
-      'bio':true
+      'bio':true,
+      "country":true
     }
   }
+  
 
   ngOnInit() {
-    this.getParticipants();
+    this.getParticipants().subscribe(participants => {
+        console.log('participants from file: ',participants);
+        this._participants = participants;
+    });
+
+    // this.getParticipantsHardcoded();
+    
   }
 
-  private getParticipants():void{
+  private getParticipants(): Observable<any> {
+    let filePath:string = './assets/colaboarthon_poets.json';
+    console.log('getParticipants')
+    return this.http.get(filePath);
+    }
+
+  private getParticipantsHardcoded():void{
     // this.insightsService.getSDGSelections(false).subscribe(this.sDGSelectionsReceived.bind(this));
     this._participants = [
       {
-        'name': 'Bob Holman',
+        'name': 'HARDCODED: Bob Holman',
         dataContent: {
-          bio:'Bob Holman created BAS .....',
+          bio:"HARDCODED: Founder of the Bowery Poetry Club and the author of 17 poetry collections (print/audio/video), most recently The Cutouts (Matisse) (PeKaBoo Press) and Sing This One Back to Me (Coffee House Press), Bob Holman has taught at Princeton, Columbia, NYU, Bard, and The New School. He is the producer/director/host of various films, including \"The United States of Poetry,\" and \"On the Road with Bob Holman.\" His film, \"Language Matters with Bob Holman,\" winner of the Berkeley Film Festival's Documentary of the Year award, was produced by David Grubin and aired nationally on PBS. He is a co-founder of the Endangered Language Alliance, and recipient of the Chambra D’oc Award for language revitalization.",
           avatar: 'assets/images/avatars/user.avatar-5bebf6ddbc7b170b18a38384.jpg'
         },
         mapId: null,
@@ -52,7 +71,7 @@ export class ParticipantsComponent implements OnInit {
       {
         'name': 'Ivanka Radmanovic',
         dataContent: {
-          bio:'Ivanka Radmanovic started her professional .....',
+          bio:'HARDCODED: Ivanka Radmanovic started her professional .....',
           avatar: 'assets/images/avatars/user.avatar-5bebf71cbc7b170b18a38388.jpg'
         },
         mapId: null,
@@ -61,7 +80,7 @@ export class ParticipantsComponent implements OnInit {
     ]
   }
 
-  //TODO: use a method for getting only this user SDGs
+  // TODO: use a method for getting only this user SDGs
   // sDGSelectionsReceived(sdgs:KEdge[]):void{
   //   console.log('sDGSelectionsReceived',sdgs);
 
