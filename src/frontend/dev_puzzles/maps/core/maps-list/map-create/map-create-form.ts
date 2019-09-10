a6d5abb54de939607138d310352987ce807e276f
Sinisa Rudan <sinisa.rudan@gmail.com>
2019-09-10 15:13:08 +0200

    creatingMapFromTemplate frontend improvements

--- src/frontend/dev_puzzles/maps/core/maps-list/map-create/map-create-form.ts
+++ src/frontend/dev_puzzles/maps/core/maps-list/map-create/map-create-form.ts
@@ -1,59 +1,63 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
 
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import {Observable} from 'rxjs';
-import { map, filter, startWith } from 'rxjs/operators';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
+import { Observable } from "rxjs";
+import { map, filter, startWith } from "rxjs/operators";
 
-import { RimaAAAService } from '@colabo-rima/f-aaa/rima-aaa.service';
-import {KnalledgeMapService} from '@colabo-knalledge/f-store_core/knalledge-map.service';
-import {MatSnackBar} from '@angular/material';
+import { RimaAAAService } from "@colabo-rima/f-aaa/rima-aaa.service";
+import { KnalledgeMapService } from "@colabo-knalledge/f-store_core/knalledge-map.service";
+import { MatSnackBar } from "@angular/material";
 
 // import { KNode } from '@colabo-knalledge/f-core/code/knalledge/kNode';
-import {KMap} from '@colabo-knalledge/f-core/code/knalledge/kMap';
+import { KMap } from "@colabo-knalledge/f-core/code/knalledge/kMap";
 
 // import {NgForm} from '@angular/forms';
 // import {Media} from "ng2-material";
 // import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
-import {MatBottomSheetRef} from '@angular/material';
-import {MAT_BOTTOM_SHEET_DATA} from '@angular/material';
+import { MatBottomSheetRef } from "@angular/material";
+import { MAT_BOTTOM_SHEET_DATA } from "@angular/material";
 
-export interface MapCreateFormData{
-    map:KMap, 
-    callback:Function
+export interface MapCreateFormData {
+  map: KMap;
+  callback: Function;
 }
 
 @Component({
-    selector: 'map-create-form',
-    templateUrl: './map-create-form.html',
-    styleUrls: ['./map-create-form.css']
+  selector: "map-create-form",
+  templateUrl: "./map-create-form.html",
+  styleUrls: ["./map-create-form.css"]
 })
 export class MapCreateForm implements OnInit {
-
-//   public selectedCountry:String;
+  //   public selectedCountry:String;
+  // public mapTemplate: any;
   form: FormGroup;
-  protected callback:Function=null;
-  public submitted:boolean = false;
+  protected callback: Function = null;
+  public submitted: boolean = false;
 
   //an exmaple of defining a form control as independet
-//   firstName:FormControl = new FormControl("", [Validators.required, Validators.minLength(2)]);
+  //   firstName:FormControl = new FormControl("", [Validators.required, Validators.minLength(2)]);
 
   constructor(
     private fb: FormBuilder,
-    private knalledgeMapService:KnalledgeMapService,
+    private knalledgeMapService: KnalledgeMapService,
     private rimaAAAService: RimaAAAService,
     private bottomSheetRef: MatBottomSheetRef<MapCreateForm>,
     @Inject(MAT_BOTTOM_SHEET_DATA) public data: MapCreateFormData,
     public snackBar: MatSnackBar
   ) {
-        this.callback = data.callback;
-        // this.mdDialog.show();
-        // this.mapFormActive = false;
-        // setTimeout(() => this.mapFormActive = true, 0.1);
-        // this.model = map;
+    this.callback = data.callback;
+    // this.mdDialog.show();
+    // this.mapFormActive = false;
+    // setTimeout(() => this.mapFormActive = true, 0.1);
+    // this.model = map;
 
-        this.bottomSheetRef.afterDismissed().subscribe(this.dissmissed.bind(this));
-        
+    this.bottomSheetRef.afterDismissed().subscribe(this.dissmissed.bind(this));
   }
 
   ngOnInit() {
@@ -62,50 +66,60 @@ export class MapCreateForm implements OnInit {
       // name: ['', [Validators.required,
       //   CustomValidators.validateCharacters //example of using custom validator imported from other service
       // ]],
-  //   "email": ['', [Validators.required, Validators.email]],
-      "name":["", [Validators.required, Validators.minLength(2)]],
-      "isPublic":[false],
+      //   "email": ['', [Validators.required, Validators.email]],
+      name: ["", [Validators.required, Validators.minLength(2)]],
+      isPublic: [false],
       // 'countryControl': [this.countries[1].id],
-      "mapTemplate":[this.mapTemplates[0].id],
-    //   "password":["", [Validators.required, Validators.minLength(3)]]
+      mapTemplate: [this.mapTemplates[0].id],
+      desc: [this.mapTemplates[0].desc]
+      //   "password":["", [Validators.required, Validators.minLength(3)]]
     });
 
     this.form.valueChanges
-    // example .map((value) => {
-    //     value.firstName = value.firstName.toUpperCase();
-    //     return value;
-    // })
-    .pipe(filter((value) => this.form.valid))
-    .subscribe((value) => {
-        console.log("Model Driven Form valid value: vm = ",
-                    JSON.stringify(value));
-    });
-  
+      // example .map((value) => {
+      //     value.firstName = value.firstName.toUpperCase();
+      //     return value;
+      // })
+      .pipe(filter(value => this.form.valid))
+      .subscribe(value => {
+        console.log(
+          "Model Driven Form valid value: vm = ",
+          JSON.stringify(value)
+        );
+      });
   }
 
-  labelPublic():string{
-    return this.form.value.isPublic ? 'public Map' : 'private Map <mat-icon>lock</mat-icon>'
+  labelPublic(): string {
+    return this.form.value.isPublic
+      ? "public Map"
+      : "private Map <mat-icon>lock</mat-icon>";
   }
 
-  isPublic():boolean{
-      return this.form.value.isPublic;
+  isPublic(): boolean {
+    return this.form.value.isPublic;
   }
 
-  get creatBtnTitle():string{
-    return this.submitted ? 'Creating ...' : 'Create';
+  get creatBtnTitle(): string {
+    return this.submitted ? "Creating ..." : "Create";
   }
-
 
   // countries = ['USA', 'Canada', 'Uk']
 
   mapTemplates = [
     {
-      id:'plain-map',
-      name:'Plain Map'
+      id: "plain-map",
+      name: "Plain Map",
+      desc: "Plain Map"
     },
     {
-      id:'co-writing',
-      name:'Collective Creative Writing'
+      id: "sust-dev",
+      name: "Sustainable Development Colabo",
+      desc: "Welcome to the Collaboration on Sustainable Development"
+    },
+    {
+      id: "co-writing",
+      name: "Collective Creative Writing",
+      desc: "Welcome to the Collective Creative Writing"
     }
   ];
 
@@ -129,66 +143,83 @@ export class MapCreateForm implements OnInit {
 
   // }
 
-  dissmissed():void{
+  public getDesc(): string {
+    if (this.form.value.mapTemplate) {
+      let mapT = this.mapTemplates.find(mapTemp => {
+        return mapTemp.id === this.form.value.mapTemplate;
+      });
+      return mapT.desc;
+    } else {
+      return "";
+    }
+  }
+
+  dissmissed(): void {
     //   console.log('[MapCreateForm] dissmissed');
   }
 
-  reset():void {
+  reset(): void {
     this.form.reset();
   }
 
-  cancel():void {
+  cancel(): void {
     this.finish(null);
   }
 
-  finish(result:KMap):void{
+  finish(result: KMap): void {
     this.form.reset(); //cleaning the form for the next use
     this.bottomSheetRef.dismiss();
-    if(this.callback){
-        this.callback(result);
+    if (this.callback) {
+      this.callback(result);
     }
     // this.submitted = false;
   }
 
-  onSubmit( ){
-    if(this.form.valid)
-    {
+  onSubmit() {
+    if (this.form.valid) {
       console.log("model-based form submitted");
       console.log(this.form);
       this.submitted = true;
-      let map:KMap = new KMap();
+      let map: KMap = new KMap();
       map.name = this.form.value.name;
       map.isPublic = this.form.value.isPublic;
       map.iAmId = this.rimaAAAService.getUserId();
       map.type = this.form.value.mapTemplate;
+      map.dataContent = { property: this.form.value.desc };
 
-      this.knalledgeMapService.create(map).subscribe(this.mapCreated.bind(this));
-    }else{
-      console.log('cannot submit! The form is not valid');
+      this.knalledgeMapService
+        .create(map)
+        .subscribe(this.mapCreated.bind(this));
+    } else {
+      console.log("cannot submit! The form is not valid");
     }
   }
 
-  mapCreated(map:KMap):void{
-    if(!map){
-        console.error('[MapCreateForm] error in creation');
-        //TODO: see if we want to show notification here, or handle it by 'callback'
-        this.snackBar.open("Map Creation Failed", "Try Again", {duration: 2000});
-    }
-    else{
-       this.finish(map);
+  mapCreated(map: KMap): void {
+    if (!map) {
+      console.error("[MapCreateForm] error in creation");
+      //TODO: see if we want to show notification here, or handle it by 'callback'
+      this.snackBar.open("Map Creation Failed", "Try Again", {
+        duration: 2000
+      });
+    } else {
+      this.snackBar.open("Map Created", "OK", {
+        duration: 2000
+      });
+      this.finish(map);
     }
   }
 
-    // myControl = new FormControl();
-    // public mapFormActive = true;
-    // model = new KMap();
+  // myControl = new FormControl();
+  // public mapFormActive = true;
+  // model = new KMap();
 
-    // openLink(event: MouseEvent): void {
-    //     this.bottomSheetRef.dismiss();
-    //     event.preventDefault();
-    // }
+  // openLink(event: MouseEvent): void {
+  //     this.bottomSheetRef.dismiss();
+  //     event.preventDefault();
+  // }
 
-    /*
+  /*
     // TODO: Remove this when we're done
     get diagnostic() { return JSON.stringify(this.model); }
 
@@ -205,7 +236,7 @@ export class MapCreateForm implements OnInit {
     }
     */
 
-    /*
+  /*
   show(map:KMap=null, callback:Function=null):void{
     console.log("[MapFormComponent].show");
     this.callback = callback;
@@ -215,7 +246,7 @@ export class MapCreateForm implements OnInit {
     // this.model = map;
   }*/
 
-    // fullUpdate() {
+  // fullUpdate() {
   //   this.form.setValue({firstName: 'Partial', password: 'monkey'});
   // }
   //
