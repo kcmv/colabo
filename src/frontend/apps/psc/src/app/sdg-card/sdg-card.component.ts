2184bebdaa37a6c6cf161287ef7a5a8e29315f92
Sinisa Rudan <sinisa.rudan@gmail.com>
2019-09-28 11:28:57 +0200

    SDG flipping done (so far with the hardcoded height); issue #474

--- src/frontend/apps/psc/src/app/sdg-card/sdg-card.component.ts
+++ src/frontend/apps/psc/src/app/sdg-card/sdg-card.component.ts
@@ -1,40 +1,42 @@
-import { Component, OnInit, EventEmitter, Input, Output  } from '@angular/core';
-import {KNode} from '@colabo-knalledge/f-core/code/knalledge/kNode';
+import { Component, OnInit, EventEmitter, Input, Output } from "@angular/core";
+import { KNode } from "@colabo-knalledge/f-core/code/knalledge/kNode";
 
 @Component({
-  selector: 'sdg-card',
-  templateUrl: './sdg-card.component.html',
-  styleUrls: ['./sdg-card.component.css']
+  selector: "sdg-card",
+  templateUrl: "./sdg-card.component.html",
+  styleUrls: ["./sdg-card.component.css"]
 })
 export class SdgCardComponent implements OnInit {
-
   @Output() toggled = new EventEmitter<boolean>();
-  @Input() disabledSelection:boolean = false;
+  @Input() disabledSelection: boolean = false;
   @Input() sdg: KNode;
-  @Input() selected:boolean = false;
-  sdgImagesPath:string = 'assets/images/sdgs/s/';
+  @Input() selected: boolean = false;
+  sdgImagesPath: string = "assets/images/sdgs/s/";
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
     //console.log('SdgCardComponent');
   }
 
-  getState():boolean{
+  getState(): boolean {
     // console.log('getState:',this.selected);
     return this.selected;
   }
 
-  getDisabled():boolean{
+  getDisabled(): boolean {
     return this.disabledSelection;
   }
 
-  onToggling():void{
+  onToggling(): void {
     this.selected = !this.selected;
-    console.log('onToggling:',this.selected);
+    console.log("onToggling:", this.selected);
 
     //TODO inform parent component:
     this.toggled.emit(this.selected);
   }
-
+  onClick(event: Event) {
+    console.log("onClick");
+    event.stopPropagation();
+  }
 }
