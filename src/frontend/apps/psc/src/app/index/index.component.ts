cc7d4224d59efa76464020c28329b383eff16346
Sinisa Rudan <sinisa.rudan@gmail.com>
2019-10-03 11:48:19 +0200

    using global & configurable App Title

--- src/frontend/apps/psc/src/app/index/index.component.ts
+++ src/frontend/apps/psc/src/app/index/index.component.ts
@@ -1,29 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import {RimaAAAService} from '@colabo-rima/f-aaa/rima-aaa.service';
-import {UserData} from '@colabo-rima/f-aaa/userData';
-import { KNode } from '@colabo-knalledge/f-core/code/knalledge/kNode';
-import {Observable} from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { RimaAAAService } from "@colabo-rima/f-aaa/rima-aaa.service";
+import { UserData } from "@colabo-rima/f-aaa/userData";
+import { KNode } from "@colabo-knalledge/f-core/code/knalledge/kNode";
+import { Observable } from "rxjs";
+import * as config from "@colabo-utils/i-config";
 
 @Component({
-  selector: 'app-index',
-  templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  selector: "app-index",
+  templateUrl: "./index.component.html",
+  styleUrls: ["./index.component.css"]
 })
 export class IndexComponent implements OnInit {
+  public appTitle: string = config.GetGeneral("appTitle");
 
-  constructor(
-    private rimaAAAService: RimaAAAService
-  ) {
-  }
+  constructor(private rimaAAAService: RimaAAAService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  get isLoggedIn():Boolean{
+  get isLoggedIn(): Boolean {
     return this.rimaAAAService.getUser() !== null;
   }
 
-  public userAvatar():Observable<string>{
+  public userAvatar(): Observable<string> {
     return RimaAAAService.userAvatar(this.rimaAAAService.getUser());
   }
 
@@ -31,12 +29,11 @@ export class IndexComponent implements OnInit {
     return this.rimaAAAService.getUser();
   }
 
-  logOut(){
+  logOut() {
     this.rimaAAAService.logOut();
   }
 
-  get isModerator(){
-    return this.rimaAAAService.isModerator();  
+  get isModerator() {
+    return this.rimaAAAService.isModerator();
   }
-
 }
