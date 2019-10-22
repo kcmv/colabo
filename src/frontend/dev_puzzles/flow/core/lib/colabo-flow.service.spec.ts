2c3620ee2cf9a31961895d2f0fde6d60c27ef90b
Sinisa Rudan <sinisa.rudan@gmail.com>
2019-10-22 22:23:52 +0200

    adding descriptions of tests to be done

--- src/frontend/dev_puzzles/flow/core/lib/colabo-flow.service.spec.ts
+++ src/frontend/dev_puzzles/flow/core/lib/colabo-flow.service.spec.ts
@@ -12,4 +12,10 @@
 //   it('should be created', inject([ColaboFlowService], (service: ColaboFlowService) => {
 //     expect(service).toBeTruthy();
 //   }));
+
+// it('should enable sending his state (whereIam, lastOnline) after login') => {
+//         //IMPORTANT: `myColaboFlowState` loading is initiated in the constructor and depends on `this.rimaAAAService.getUserId()` that is unavailable for the not logged in user
+//         //TODO
+//         expect(true).toBeTruthy();
+//       }));
 // });
--- src/frontend/dev_puzzles/moderation/core/moderation-panel/insights/user-actions-statuses/user-actions-statuses.component.spec.ts
+++ src/frontend/dev_puzzles/moderation/core/moderation-panel/insights/user-actions-statuses/user-actions-statuses.component.spec.ts
@@ -22,4 +22,21 @@
 //   it('should create', () => {
 //     expect(component).toBeTruthy();
 //   });
+
+// it("should get expected `lastOnlineDiff` and `lastOnline` from the backend ", () => {
+//   //this is important because it depends on setting of `updatedAt` in creation time and on generation of `lastOnline` in retreiving time; and also because it will be migrated in Mongoose middleware hooks
+//   //TODO:
+//   expect(true).toBeTruthy();
+// });
+
+// it("should show correct text and icon for `lastOnlineDiff` and `lastOnline` ", () => {
+//   //TODO:
+//   expect(true).toBeTruthy();
+// });
+
+// it("should show warning and NO selected SDgs for a user who is NOT logged in", () => {
+//   //TODO:
+//   expect(true).toBeTruthy();
+// });
+
 // });
