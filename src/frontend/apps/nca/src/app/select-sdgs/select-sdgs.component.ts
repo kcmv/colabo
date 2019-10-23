fd7088637e93456256629edaaf0c717e44168494
Sinisa Rudan <sinisa.rudan@gmail.com>
2019-10-23 19:52:39 +0200

    - deleted dialog from the NCA (related to "remove NCA app and create it as a fresh Puzzle-built #503"); select-sdg of NCA pretifiyed

--- src/frontend/apps/nca/src/app/select-sdgs/select-sdgs.component.ts
+++ src/frontend/apps/nca/src/app/select-sdgs/select-sdgs.component.ts
@@ -1,32 +1,35 @@
-import { MatDialog, MatDialogRef } from '@angular/material';
-import {Dialog1Btn, Dialog2Btn, DialogData} from '../util/dialog';
+import { MatDialog, MatDialogRef } from "@angular/material";
+import { Dialog, DialogData } from "@colabo-utils/f-notifications";
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import {SDGsService, SDG_SELECTION_NAME, SDG_SELECTION_TYPE, SDGS_TO_SELECT } from './sdgs.service'
-import {RimaAAAService} from '@colabo-rima/f-aaa/rima-aaa.service';
-import {KNode} from '@colabo-knalledge/f-core/code/knalledge/kNode';
-import {KEdge} from '@colabo-knalledge/f-core/code/knalledge/kEdge';
+import {
+  SDGsService,
+  SDG_SELECTION_NAME,
+  SDG_SELECTION_TYPE,
+  SDGS_TO_SELECT
+} from "./sdgs.service";
+import { RimaAAAService } from "@colabo-rima/f-aaa/rima-aaa.service";
+import { KNode } from "@colabo-knalledge/f-core/code/knalledge/kNode";
+import { KEdge } from "@colabo-knalledge/f-core/code/knalledge/kEdge";
 
 @Component({
-  selector: 'app-select-sdgs',
-  templateUrl: './select-sdgs.component.html',
-  styleUrls: ['./select-sdgs.component.css']
+  selector: "app-select-sdgs",
+  templateUrl: "./select-sdgs.component.html",
+  styleUrls: ["./select-sdgs.component.css"]
 })
 export class SelectSdgsComponent implements OnInit {
-
   // mprinc: added to avoid AOT error
-  sdgs:any[] = [];
-  saved:boolean = false;
+  sdgs: any[] = [];
+  saved: boolean = false;
   dialogRef: any; //TODO: type: MatDialogRef;
 
-
-  selectedSDGs:string[] = [];
+  selectedSDGs: string[] = [];
   constructor(
     private rimaAAAService: RimaAAAService,
     private sDGsService: SDGsService,
     public dialog: MatDialog
-  ) { }
+  ) {}
 
   ngOnInit() {
     //TODO: add loading of user's sdgs selections (and setting up states of sdg-cards) - if the user has already selected them in a previous session
@@ -42,12 +45,12 @@ export class SelectSdgsComponent implements OnInit {
     // }];
     //TODO: !! we should migrate to the App-persisten Service this server-loads. RIGHT NOW each time we open this component, it loads it:
     this.sDGsService.getSDGs().subscribe(this.sdgsReceived.bind(this));
-      //.subscribe(sdgs => this.sdgs);
+    //.subscribe(sdgs => this.sdgs);
     //this.sdgs = this.sDGsService.getSDGs();
     //this.sDGsService.loadSDGs();
   }
 
-  get isLoggedIn():Boolean{
+  get isLoggedIn(): Boolean {
     return this.rimaAAAService.getUser() !== null;
   }
 
@@ -55,79 +58,84 @@ export class SelectSdgsComponent implements OnInit {
     return this.rimaAAAService.getUser();
   }
 
-  openDialog(buttons:number, data:DialogData, options:any = null, afterClosed:Function = null): void {
-    if(options === null){
-      options = {};
-    }
-    options['width'] = '95%'
-    options['data'] = data;
-    console.log('openDialog',options);
-    this.dialogRef = this.dialog.open((buttons == 1 ? Dialog1Btn : Dialog2Btn), options);
-    if(afterClosed){this.dialogRef.afterClosed().subscribe(afterClosed);}
-  }
-
-  correctSelection():boolean{
+  correctSelection(): boolean {
     return this.selectedSDGs.length == SDGS_TO_SELECT;
   }
 
-  getActionMessage():string{
-    let msg:string='';
-    if(this.saved){
-      msg = 'you have finished this phase';
-    }
-    else{
-      if(this.selectedSDGs.length < SDGS_TO_SELECT){
-          msg = 'Select ' + (SDGS_TO_SELECT - this.selectedSDGs.length) + ' more SDGs';
+  getActionMessage(): string {
+    let msg: string = "";
+    if (this.saved) {
+      msg = "you have finished this phase";
+    } else {
+      if (this.selectedSDGs.length < SDGS_TO_SELECT) {
+        msg =
+          "Select " +
+          (SDGS_TO_SELECT - this.selectedSDGs.length) +
+          " more SDGs";
       } else if (this.selectedSDGs.length == SDGS_TO_SELECT) {
-          msg = 'Great! Please, submit ';
+        msg = "Great! Please, submit ";
       } else {
-          msg = 'Too many SDGs selected! ' + SDGS_TO_SELECT + ' SDGs, not ' + this.selectedSDGs.length;
+        msg =
+          "Too many SDGs selected! " +
+          SDGS_TO_SELECT +
+          " SDGs, not " +
+          this.selectedSDGs.length;
       }
     }
     return msg;
   }
 
-  canSubmit():boolean{
-      return !this.saved && this.correctSelection();
+  canSubmit(): boolean {
+    return !this.saved && this.correctSelection();
   }
 
-  onSubmit(){
+  onSubmit() {
     this.saved = false;
-    console.log('submit');
-    this.openDialog(1, new DialogData('Submitting','please wait ...', 'Cancel', null, true), {disableClose: true}
-    , function(){
-      console.log('The dialog NEW was closed');
-      //this.animal = result;
-    }
+    console.log("submit");
+    this.dialogRef = Dialog.open(
+      1,
+      new DialogData("Submitting", "please wait ...", "Cancel", null, true),
+      { disableClose: true },
+      function() {
+        console.log("The dialog NEW was closed");
+        //this.animal = result;
+      }
     );
-    this.sDGsService.saveSDGsSelection(this.selectedSDGs).subscribe(this.sdgsSaved.bind(this));
+    this.sDGsService
+      .saveSDGsSelection(this.selectedSDGs)
+      .subscribe(this.sdgsSaved.bind(this));
   }
 
-  private sdgsSaved():void{
-    console.log('SelectSdgsComponent::sdgsSaved');
+  private sdgsSaved(): void {
+    console.log("SelectSdgsComponent::sdgsSaved");
     this.saved = true; //TODO: see if we want to keep this
     this.dialogRef.close();
-    this.openDialog(1, new DialogData('Submitted','Thank you for your SDGs selection. You\'ve finished this phase', 'OK'));
+    this.dialogRef = Dialog.open(
+      1,
+      new DialogData(
+        "Submitted",
+        "Thank you for your SDGs selection. You've finished this phase",
+        "OK"
+      )
+    );
     //window.alert("Your selection is successfully saved");
   }
 
-  private sdgsReceived(sdgsD:any[]):void{
+  private sdgsReceived(sdgsD: any[]): void {
     this.sdgs = sdgsD;
     // for (var sdg in this.sdgs) {
     // console.log(item); // 0,1,2
   }
-    // console.log('sdgsReceived:', this.sdgs)
+  // console.log('sdgsReceived:', this.sdgs)
 
-  onToggled(state: boolean, id:string) {
-    console.log('SelectSdgsComponent::onToggled',state, id);
-    if(state){
+  onToggled(state: boolean, id: string) {
+    console.log("SelectSdgsComponent::onToggled", state, id);
+    if (state) {
       this.selectedSDGs.push(id);
-    }
-    else{
+    } else {
       let index = this.selectedSDGs.indexOf(id);
       if (index !== -1) this.selectedSDGs.splice(index, 1);
     }
     console.log(this.selectedSDGs.toString());
   }
-
 }
